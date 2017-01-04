b6cfe92d14578b225d7797f09dfa81959d7dcadf
Will <willmercedes@gmail.com>
2017-01-04 20:29:55 +0800

    ImageNet
    Lightweight 07

--- caae/src/ofApp.cpp
+++ caae/src/ofApp.cpp
@@ -1,27 +1,4 @@
 #include "ofApp.h"
-#include "ofxTrutypeFontUC.h"
-#include "ofxTween.h"
-#include "Lyrics.hpp"
-
-#define W 720
-#define H 480
-#define NUM 10
-#define FROM 500
-#define TO 0
-#define DURATION 1000
-
-//font
-ofxTrueTypeFontUC myFont;
-string sampleString;
-vector<ofPath> characters;
-vector<ofMesh> mesh;
-vector<ofMesh> newMesh;
-vector<int> ran;
-ofPoint center;
-Lyrics lyrics;
-ofDirectory dir;
-vector<string> fonts;
-
 
 void ofApp::setup(){
     
