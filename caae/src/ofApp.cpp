5d067939774d1c4625b0d5f577bc01d948ddb5db
Will <willmercedes@gmail.com>
2017-01-06 01:43:38 +0800

    ImageNet
    Lightweight 06

--- caae/src/ofApp.cpp
+++ caae/src/ofApp.cpp
@@ -1,23 +1,4 @@
-#include "ofApp.h"
 
-int numPoints = 1500;
-int numSpheres = 3;
-int lengthLimit = 80;
-
-void ofApp::setup(){
-    
-}
-
-void ofApp::draw(){
-    
-    ofBackground(255);
-    ofTranslate(ofGetWidth/2, ofGetHeight/2);
-
-}
-
-void ofApp::update(){
-    
-}
 
 
 
