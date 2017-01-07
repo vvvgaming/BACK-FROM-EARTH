5ffa7349a8681bf7a71121b62bf49f04f83e1abc
Will <willmercedes@gmail.com>
2017-01-08 01:17:28 +0800

    ImageNet
    Lightweight 01

---
+++ caae/src/flower.hpp
@@ -0,0 +1,19 @@
+//
+//  flower.hpp
+//  caae
+//
+//  Created by 刘志健 on 17/1/7.
+//
+//
+
+#ifndef flower_hpp
+#define flower_hpp
+
+#include "ofMain.h"
+
+struct Branch{
+    
+
+}
+
+#endif /* flower_hpp */
