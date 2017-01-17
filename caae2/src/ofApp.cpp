0efe0358ee4fd2da9a844a969912068988419c2a
Will <willmercedes@gmail.com>
2017-01-18 00:32:09 +0800

    ImageNet
    Lightweight 02

--- caae2/src/ofApp.cpp
+++ caae2/src/ofApp.cpp
@@ -2,6 +2,29 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    
+    ofShowCursor();
+    
+    ofSetFrameRate(30);
+    
+    jpeg.setup(20, 40, 80);
+    
+    shader.load("shaders/shader.vert", "shaders/shader.frag");
+    
+    gui.setup();
+    gui.add(ratioThreshold.setup( "ratio", 0.75f, 0, 1 ));
+    
+    setupImages("images", "png");
+    
+    tracker.setup();
+    tracker.setRescale(.5);
+    classifier.reset();
+    classifierInited = false;
+    
+    grabber.initGrabber(640, 480);
+    
+    imageIndex = 0;
+    glitchCount = 0;
 
 }
 
