4d4a5757a50b70b56252acf852f02644072a1e42
Will <willmercedes@gmail.com>
2017-01-23 07:09:00 +0800

    ImageNet
    Lightweight 08

--- caae2/src/ofApp.cpp
+++ caae2/src/ofApp.cpp
@@ -1,189 +1,110 @@
 #include "ofApp.h"
+#include "ofxPubSubOsc.h"
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	ofSetVerticalSync(true);
+
+	ofBackground(255);
+	ofSetLogLevel(OF_LOG_VERBOSE);
+
+    fsrThread = ofPtr<SerialThread>(new SerialThread("/dev/cu.usbserial-A6004b3G"));
+    contactThread = ofPtr<SerialThread>(new SerialThread("/dev/cu.usbmodemfd1311"));
+    fsrThread->startThread(true);
+    contactThread->startThread(true);
     
-    //DBA
+    fsrTiles.resize(6, 0);
+    fsrRaw.resize(16, 0);
+    ofxPublishOsc("localhost", 14924, "/niw/client/VtoF", fsrTiles, false);
+    ofxPublishOsc("localhost", 14925, "/niw/client/raw", fsrRaw, false);
     
-    ofShowCursor();
+    sender.setup("localhost", 14924);
+    
+    hapticPresets.resize(4, None);
     
     ofSetFrameRate(30);
-    
-    jpeg.setup(20, 40, 80);
-    
-    shader.load("shaders/shader.vert", "shaders/shader.frag");
-    
-    gui.setup();
-    gui.add(ratioThreshold.setup( "ratio", 0.75f, 0, 1 ));
-    
-    setupImages("images", "png");
-    
-    tracker.setup();
-    tracker.setRescale(.5);
-    classifier.reset();
-    classifierInited = false;
-    
-    grabber.initGrabber(640, 480);
-    
-    imageIndex = 0;
-    glitchCount = 0;
-
-}
-
-void ofApp::setupImages(string dirName, string ext) {
-    // load images in images folder
-    ofDirectory dir(dirName);
-    dir.allowExt(ext);
-    dir.listDir();
-    for(int i = 0; i < dir.size(); i++) {
-        images.push_back(ofImage());
-        images.back().load(dirName + "/" + dir.getName(i));
-    }
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
     
-    ofSetWindowTitle(ofToString(ofGetFrameRate()));
-    
-    grabber.update();
-    if(grabber.isFrameNew()) {
-        tracker.update(ofxCv::toCv(grabber));
-    }
-    
-    if(tracker.getFound() && !classifierInited) {
-        classifier.load("expressions");
-        classifierInited = true;
-    }
-    
-    updateDecision();
-    updateGlitch();
-
-}
-
-void ofApp::updateDecision(){
-    classifier.classify(tracker);
-    
-    // decide what glitch we want
-    int primary = classifier.getPrimaryExpression();
-    float pTotal = 0;
-    float pPrime = classifier.getProbability(primary);
-    for(int i = 0; i < classifier.size(); i++){
-        pTotal += classifier.getProbability(i);
-    }
-    
-    // check if the result is not confident enough
-    // (Strictly speaking this is too optimistic, and instead entropy should be used.
-    //  But since we only have 3 labels, this is not a huge issue.)
-    if(pPrime / pTotal < ratioThreshold) {
-        // if eyebrows are raised rather than smiling, apply gray
-        if(classifier.getProbability(0) > classifier.getProbability(2)) {
-            glitchType = GRAY;
-        }
-        // if smiling
-        else {
-            glitchType = JPEG;
+    for (int i = 0; i < 4; i++) {
+        fsrTiles.at(i) = 0;
+        for (int j = 0; j < 4; j++) {
+            ofVec2f v;
+            int val = fsrThread->getSensorValue(i, j, v);
+            fsrTiles.at(i) += val * 100; // scaling
+            fsrRaw.at(i * 4 + j) = val * 100; // scaling
         }
     }
-    // confident enough; no glitch
-    else {
-        glitchType = NONE;
-    }
-}
-
-//--------------------------------------------------------------
-void ofApp::updateGlitch(){
-    // countdown; lasts for 10 frames
-    glitchCount = max(0, glitchCount - 1);
     
-    if(glitchType == JPEG) {
-        if(imageIndex == 0) {
-            imageIndex = ofRandom(1, images.size());
-            glitchCount = 10;
+    for (int i = 0; i < 4; i++) {
+        stringstream ss;
+        ss << static_cast<char>('a' + i) << ' ';
+        vector<int> vals;
+        for (int j = 0; j < 4; j++) {
+            ofVec2f v;
+            int val = contactThread->getSensorValue(i, j, v);
+            vals.push_back(val);
+            ss << val << ' ';
         }
-        if(!jpeg.getImage().isAllocated() || ofGetFrameNum() % 5 == 0) {
-            jpeg.setPixels(images.at(imageIndex - 1).getPixelsRef());
-            jpeg.glitch();
+        //ofLogError() << ss.str();
+        
+        ofxOscMessage message;
+        message.setAddress("/niw/preset");
+        message.addIntArg(i + 1);
+        if(hapticPresets[i] != Ice && vals.at(0) == 0 && vals.at(1) == 1 && vals.at(2) == 0 && vals.at(3) == 0)
+        {
+            hapticPresets[i] = Ice;
+            message.addStringArg("ice");
+            sender.sendMessage(message);
+            system(("say " + ofToString(static_cast<char>('a' + i)) + " ice").c_str());
+        }
+        else if(hapticPresets[i] != Sand && vals.at(0) == 0 && vals.at(1) == 0 && vals.at(2) == 0 && vals.at(3) == 1)
+        {
+            hapticPresets[i] = Sand;
+            message.addStringArg("sand");
+            sender.sendMessage(message);
+            system(("say " + ofToString(static_cast<char>('a' + i)) + " sand").c_str());
+        }
+        else if(hapticPresets[i] != Water && vals.at(0) == 0 && vals.at(1) == 0 && vals.at(2) == 1 && vals.at(3) == 0)
+        {
+            hapticPresets[i] = Water;
+            message.addStringArg("water");
+            sender.sendMessage(message);
+            system(("say " + ofToString(static_cast<char>('a' + i)) + " water").c_str());
+        }
+        else if(hapticPresets[i] != Can && vals.at(0) == 1 && vals.at(1) == 0 && vals.at(2) == 0 && vals.at(3) == 0)
+        {
+            hapticPresets[i] = Can;
+            message.addStringArg("can");
+            sender.sendMessage(message);
+            system(("say " + ofToString(static_cast<char>('a' + i)) + " can").c_str());
         }
     }
-    else {
-        imageIndex = 0;
-    }
-}
-
-//--------------------------------------------------------------
-void ofApp::exit() {
-    tracker.waitForThread();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    return;
     
-    ofBackground(0);
-    ofSetColor(255, 255);
+	ofSetColor(80);
+	ofBackground(255);
+	ofFill();
+	ofTranslate(200, 400);
+	ofScale(1, -1);
     
-    shader.begin();
-    ofBaseDraws *baseDraw;
-    
-    switch(glitchType) {
-        case GRAY:
-            // draw camera feed in gray
-            baseDraw = &grabber;
-            shader.setUniformTexture("tex0", grabber.getTextureReference(), 0);
-            shader.setUniform1f("doGray", 1);
-            break;
-        case JPEG:
-            // draw images with glitch
-            baseDraw = &(jpeg.getImage());
-            shader.setUniformTexture("tex0", jpeg.getImage().getTextureReference(), 0);
-            shader.setUniform1f("doGray", 0);
-            break;
-        case NONE:
-        default:
-            // draw camera feed in color
-            baseDraw = &grabber;
-            shader.setUniformTexture("tex0", grabber.getTextureReference(), 0);
-            shader.setUniform1f("doGray", 0);
-            break;
-    }
-    
-    shader.setUniform2f("imageViewPort", baseDraw->getWidth(), baseDraw->getHeight());
-    shader.setUniform2f("screenViewPort", ofGetWidth(), ofGetHeight());
-    
-    // draw empty rect (fragment shader will map tex0 i.e. baseDraw)
-    ofRect(0, 0, ofGetWidth(), ofGetHeight());
-    
-    shader.end();
-    
-    gui.draw();
-    drawClassifier();
-
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			ofVec2f v;
+			int val = fsrThread->getSensorValue(i, j, v);
+			ofDrawCircle(v * 100, val / 10.0);
+		}
+	}
 }
 
-
-void ofApp::drawClassifier() {
-    int w = 100, h = 12;
-    ofPushStyle();
-    ofPushMatrix();
-    ofTranslate(5, 100);
-    int n = classifier.size();
-    int primary = classifier.getPrimaryExpression();
-    for(int i = 0; i < n; i++){
-        ofSetColor(i == primary ? ofColor::red : ofColor::black);
-        ofRect(0, 0, w * classifier.getProbability(i) + .5, h);
-        ofSetColor(255);
-        ofDrawBitmapString(classifier.getDescription(i), 5, 9);
-        ofTranslate(0, h + 5);
-    }
-    ofPopMatrix();
-    ofPopStyle();
-}
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-    
-    if(key == 'f') {
-        ofToggleFullscreen();
-    }
 
 }
 
@@ -193,7 +114,7 @@ void ofApp::keyReleased(int key){
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y){
 
 }
 
@@ -213,16 +134,6 @@ void ofApp::mouseReleased(int x, int y, int button){
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
 
 }
@@ -233,6 +144,7 @@ void ofApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo){
 
 }
+
