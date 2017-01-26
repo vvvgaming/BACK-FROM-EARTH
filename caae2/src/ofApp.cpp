47751c16f7c7aec93bd04c6b8bdd82d8d502649f
Will <willmercedes@gmail.com>
2017-01-27 00:02:44 +0800

    ImageNet
    Lightweight 02

--- caae2/src/ofApp.cpp
+++ caae2/src/ofApp.cpp
@@ -105,37 +105,3 @@ void ofApp::draw(){
 
 
 
-
-
-
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){
-
-}
-
