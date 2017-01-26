1b451ff3981a12b962cc5cf87481fca731140aa5
Will <willmercedes@gmail.com>
2017-01-27 00:03:12 +0800

    ImageNet
    Lightweight 03

--- caae2/src/ofApp.h
+++ caae2/src/ofApp.h
@@ -138,16 +138,6 @@ public:
 	void update();
 	void draw();
 
-	void keyPressed(int key);
-	void keyReleased(int key);
-	void mouseMoved(int x, int y);
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void windowResized(int w, int h);
-	void dragEvent(ofDragInfo dragInfo);
-	void gotMessage(ofMessage msg);
-
 	ofPtr<SerialThread> fsrThread, contactThread;
     
     enum HapticPreset {None, Ice, Snow, Can, Water, Sand};
