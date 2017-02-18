59de5f48021741f9549ef9658b4d7d5af3d55979
Will <willmercedes@gmail.com>
2017-02-19 07:11:16 +0800

    ImageNet
    Lightweight 05

--- FX5855/src/main.cpp
+++ FX5855/src/main.cpp
@@ -5,9 +5,6 @@ int main( ){
     //	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
     
     // 720p
-    ofSetupOpenGL(960,720,OF_WINDOW);
-    
-    // this kicks off the running of my app
 
     ofRunApp(new ofApp());
     
