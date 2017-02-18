a33626e02f7ab2fc6d78d1b63ee443ede988f264
Will <willmercedes@gmail.com>
2017-02-19 07:08:42 +0800

    ImageNet
    Lightweight 03

--- FX5855/src/main.cpp
+++ FX5855/src/main.cpp
@@ -8,8 +8,7 @@ int main( ){
     ofSetupOpenGL(960,720,OF_WINDOW);
     
     // this kicks off the running of my app
-    // can be OF_WINDOW or OF_FULLSCREEN
-    // pass in width and height too:
+
     ofRunApp(new ofApp());
     
 }
