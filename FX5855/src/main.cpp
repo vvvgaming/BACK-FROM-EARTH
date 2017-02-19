2cddb500e3e7b85e923f00f59a746508cbd4022f
Will <willmercedes@gmail.com>
2017-02-20 07:33:23 +0800

    ImageNet
    Lightweight 01

--- FX5855/src/main.cpp
+++ FX5855/src/main.cpp
@@ -1,4 +1,11 @@
 #include "ofMain.h"
 #include "ofApp.h"
 
-
+int main( )
+{
+    ofGLWindowSettings settings_;
+    settings_.setGLVersion(3, 2);
+    ofCreateWindow(settings_);
+    
+    ofRunApp(new ofApp());
+}
