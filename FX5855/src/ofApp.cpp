36dcc047f2150bd7b4b3c944e0900e47384f21f1
Will <willmercedes@gmail.com>
2017-02-21 00:01:29 +0800

    ImageNet
    Lightweight 14

--- FX5855/src/ofApp.cpp
+++ FX5855/src/ofApp.cpp
@@ -2,14 +2,7 @@ void ofApp::setup() {
     // setup OF stuff
     ofSetCircleResolution(50);
     
-    // build graph
-    //    graph = Graph::buildSimpleGraph();
-    //    graph = Graph::buildRandomGraph();
-    graph = Graph::buildProductionGraph();
-    
-    // backref to app
-    graph->setApp(this);
-}
+ 
 
 
  
