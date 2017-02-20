f5e4026c1a6e424d3af0d39726b70ca5df6fc5c0
Will <willmercedes@gmail.com>
2017-02-20 23:59:31 +0800

    ImageNet
    Lightweight 06

--- FX5855/src/ofApp.cpp
+++ FX5855/src/ofApp.cpp
@@ -28,16 +28,7 @@ void ofApp::keyReleased(int key) {}
 
 void ofApp::mouseMoved(int x, int y) {
     // set center gravity
-    if (x < 10) {
-        graph->center = false;
-    } else if (x > ofGetWindowWidth() - 10) {
-        graph->center = true;
-    }
-    if (y < 10) {
-    } else if (y > ofGetWindowHeight() - 10) {
-        for (auto ln : graph->getLanguageNodes()) {
-            ln->pinned = false;
-        }
+
 
 
 
