98b88a336684e7ef5e3fdd03b3610cea824a1a09
Will <willmercedes@gmail.com>
2017-02-22 07:19:09 +0800

    ImageNet
    Lightweight 10

--- FX5855/src/ofApp.cpp
+++ FX5855/src/ofApp.cpp
@@ -9,13 +9,8 @@ void ofApp::setup()
     ofSetCircleResolution(128);
     ofEnableSmoothing();
     
-    // prepare original image
-    image.load("IMG_7223.JPG");
-    image.resize(100, 100);
-    image.setAnchorPercent(0.5, 0.5);
-    
+}
 
- 
 
 
 
