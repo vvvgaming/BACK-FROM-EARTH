be9e8888c38150b9203e5450700de3eeebb3c877
Will <willmercedes@gmail.com>
2017-02-24 07:32:18 +0800

    ImageNet
    Lightweight 01

--- FX5855/src/ofApp.cpp
+++ FX5855/src/ofApp.cpp
@@ -3,7 +3,17 @@
 
 
 
+void ofApp::setup(){
+    ofShowCursor();
+    
+    ofSetFrameRate(30);
+    
+    jpeg.setup(20, 40, 80);
+    
+    shader.load("shaders/shader.vert", "shaders/shader.frag");
 
+
+}
 
 
 
