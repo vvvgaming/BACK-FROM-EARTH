691cf0cacd3bafc3c534d99b94f6f9ed84457f46
Will <willmercedes@gmail.com>
2017-02-26 07:08:20 +0800

    ImageNet
    Lightweight 06

--- FX5855/src/ofApp.h
+++ FX5855/src/ofApp.h
@@ -26,3 +26,7 @@ public:
     void windowResized(int w, int h);
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
+
+private:
+    ofVideoGrabber grabber;
+    ofxFaceTrackerThreaded tracker;
