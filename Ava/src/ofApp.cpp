6c7404e277216ef8e9df922bda62ce75d792c0bb
Will <willmercedes@gmail.com>
2017-04-03 16:18:55 +0800

    ImageNet
    Lightweight 10

--- Ava/src/ofApp.cpp
+++ Ava/src/ofApp.cpp
@@ -40,11 +40,6 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
 
 
 
