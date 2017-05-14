5f8b5d384744b51c5dfa2b69091178dd69a84d87
Will <willmercedes@gmail.com>
2017-05-15 00:13:38 +0800

    ImageNet
    Lightweight 09

--- Digital/src/main.cpp
+++ Digital/src/main.cpp
@@ -1,6 +1,6 @@
 #include "ofMain.h"
 #include "ofApp.h"
-//========================================================================
+
 int main( ){
 	ofSetupOpenGL(1024,768,OF_WINDOW);			
 	// this kicks off the running of my app
