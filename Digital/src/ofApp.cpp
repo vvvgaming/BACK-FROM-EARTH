90cbce29d6ae4938a07aa3f781400c7c78e2b087
Will <willmercedes@gmail.com>
2017-05-16 08:59:09 +0800

    ImageNet
    Lightweight 06

--- Digital/src/ofApp.cpp
+++ Digital/src/ofApp.cpp
@@ -4,8 +4,19 @@ void ofApp::setup(){
     
     ofBackground(0);
     ofSetloglevel("ofxCsv", OF_LOG_VERBOSE);
-    
     ofSetCircleResolution(3);
+    
+    if(csv.load("airports-extended.dat")){
+        
+        for(int i = 0; i < csv.getNumRows(); i++){
+            for(int j = 0; j < csv.getNumCols(i); j++){
+            
+            }
+        
+        }
+    
+    }
+    
 
 }
 
