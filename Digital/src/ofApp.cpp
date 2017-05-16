a7f889c0e553b6ae69b13fca4f4e81b6540e9f80
Will <willmercedes@gmail.com>
2017-05-16 09:08:57 +0800

    ImageNet
    Lightweight 10

--- Digital/src/ofApp.cpp
+++ Digital/src/ofApp.cpp
@@ -37,7 +37,6 @@ void ofApp::draw(){
         ofxCsvRow row = csv[i];
         string type = row.getString(12);
         if(type == "airport"){
-            
             ofSetColor(31, 127, 255);
         
         }else if
