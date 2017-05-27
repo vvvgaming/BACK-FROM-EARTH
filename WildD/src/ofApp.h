25d8b9c03eb75bebb9d38a768fe73e8b38ac889f
Will <willmercedes@gmail.com>
2017-05-27 08:26:09 +0800

    ImageNet
    Lightweight 03

--- WildD/src/ofApp.h
+++ WildD/src/ofApp.h
@@ -12,9 +12,7 @@ class ofApp : public ofBaseApp{
 		void keyPressed(int key);
 		void keyReleased(int key);
 		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-
-
+	
 
 
 		
