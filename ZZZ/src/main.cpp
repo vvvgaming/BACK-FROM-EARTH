35e71cb0733a0d7e01f16f16ea530bffb429a523
Will <willmercedes@gmail.com>
2017-06-10 09:11:01 +0800

    ImageNet
    Lightweight 09

--- ZZZ/src/main.cpp
+++ ZZZ/src/main.cpp
@@ -3,9 +3,7 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
-
-
+	ofSetupOpenGL(1024,768,OF_WINDOW);			// 
 	ofRunApp(new ofApp());
 
 }
