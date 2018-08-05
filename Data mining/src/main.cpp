2aad7a4a512e9f388005bc0f673d5cd37cbb387b
Will <willmercedes@gmail.com>
2018-08-06 01:37:21 +0800

    ImageNet
    Lightweight 13

--- Data mining/src/main.cpp
+++ Data mining/src/main.cpp
@@ -3,4 +3,4 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL con
+	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL co
