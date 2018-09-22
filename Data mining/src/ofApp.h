56b306b5a586f3841887b18624bde5cbc78c7fe9
Will <willmercedes@gmail.com>
2018-09-23 07:24:49 +0800

    ImageNet
    Lightweight 07

--- Data mining/src/ofApp.h
+++ Data mining/src/ofApp.h
@@ -14,4 +14,4 @@ class ofApp : public ofBaseApp{
 		void mouseMoved(int x, int y );
 		void mouseDragged(int x, int y, int button);
 		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int
+		void mouseReleased(int x, in
