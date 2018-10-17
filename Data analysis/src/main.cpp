d604b458f08d437b10f0c9630adfc5dbf4729b04
Will <willmercedes@gmail.com>
2018-10-18 05:02:45 +0800

    ImageNet
    Lightweight 10

--- Data analysis/src/main.cpp
+++ Data analysis/src/main.cpp
@@ -3,4 +3,4 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL c
+	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL 
