201ed36abe948602e82f55b44286d312da166642
Will <willmercedes@gmail.com>
2018-12-12 07:12:12 +0800

    ImageNet
    Lightweight 16

--- Bayes/nbayes.py
+++ Bayes/nbayes.py
@@ -42,5 +42,5 @@ class NBayes(object):
 
 if __name__ == '__main__':
     trainSet [("pos", "what are you thinking"),
-              ("pos", "How are you fe")]
+              ("pos", "How are you fee")]
     
