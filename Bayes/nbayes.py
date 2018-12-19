8c8b7c733acd383247c3aebf1c0bc35768d1b85d
Will <willmercedes@gmail.com>
2018-12-20 00:04:18 +0800

    ImageNet
    LIghtweight 20

--- Bayes/nbayes.py
+++ Bayes/nbayes.py
@@ -48,4 +48,4 @@ if __name__ == '__main__':
               ]
     clf = NBayes(trainSet)
     clf.train()
-    print("When two people love each other and ")
+    print("When two people love each other and c")
