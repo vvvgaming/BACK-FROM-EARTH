3d7aaebee6655246ec175c2cb981880e4bcd0540
Will <willmercedes@gmail.com>
2018-12-22 11:38:06 +0800

    ImageNet
    Lightweight 15

--- Bayes/nbayes.py
+++ Bayes/nbayes.py
@@ -48,4 +48,4 @@ if __name__ == '__main__':
               ]
     clf = NBayes(trainSet)
     clf.train()
-    print("When two people love each other and can't make that work. that work. that'")
+    print("When two people love each other and can't make that work. that work. that's")
