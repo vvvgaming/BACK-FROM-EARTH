559310bdee8dc2a9e9aa2520846dccdcc6f75fd9
Will <willmercedes@gmail.com>
2019-02-15 11:04:15 +0800

    Imageworks
    Lightwieght 11

--- Bayes/tfidf_nbayes.py
+++ Bayes/tfidf_nbayes.py
@@ -16,33 +16,33 @@ class Corpus(object):
 
     def process_data(self, docs):
         vocabs = set()
-    for tag, doc in docs:
-        words = self.tokenizer(doc)
-        if len(words) == 0:
-           continue
-        self.tags[tag] += 1
-        self.total += 1
-        self.docs.append((tag,words))
-        vocabs.update(words)
-    vocabs = list(vocabs)
-    self.word2idx = dict(zip(vocabs, range(len(vocabs))))
+        for tag, doc in docs:
+            words = self.tokenizer(doc)
+            if len(words) == 0:
+               continue
+            self.tags[tag] += 1
+            self.total += 1
+            self.docs.append((tag,words))
+            vocabs.update(words)
+        vocabs = list(vocabs)
+        self.word2idx = dict(zip(vocabs, range(len(vocabs))))
 
 
-def calc_bow(self):
-    bow = np.zeros([self.total, len(self.word2idx)])
+    def calc_bow(self):
+        bow = np.zeros([self.total, len(self.word2idx)])
 
-    for docidx, (tag, doc) in enumerate(self.docs):
-        for word in doc:
-            bow[docidx, self.word2idx[word]] += 1
-       return bow
+        for docidx, (tag, doc) in enumerate(self.docs):
+            for word in doc:
+                bow[docidx, self.word2idx[word]] += 1
+        return bow
 
 
-def calc_tfidf(self):
-    tf = self.calc_bow()
-    df = np.ones([1, len(self.word2idx)])
+    def calc_tfidf(self):
+        tf = self.calc_bow()
+        df = np.ones([1, len(self.word2idx)])
 
-    for docidx, (tag, doc) in enumberate(self.docs):
-        tf[docidx] /= np.max(tf[docidx])
+        for docidx, (tag, doc) in enumberate(self.docs):
+            tf[docidx] /= np.max(tf[docidx])
    
    
 
