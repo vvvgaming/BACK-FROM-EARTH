d55325fc45cf9473f9ad4e47b29239408b80e40b
Will <willmercedes@gmail.com>
2019-02-21 11:35:31 +0800

    Imageworks
    Lightweight 01

--- Bayes/tfidf_nbayes.py
+++ Bayes/tfidf_nbayes.py
@@ -50,6 +50,55 @@ class Corpus(object):
 
 
     def get_vec(self, words):
+        vec = np.zeros([1, len(self.word2idx)])
+        for word in words:
+            if word in self.word2idx:
+                vec[0, self.word2idx[word]] += 1
+        return vec
+
+class NBayes(Corpus):
+    def __init__(self, docs, kernel='tfidf'):
+        super(NBayes, self).__init__()
+        self.kernel = kernel
+        self.process_data(docs)
+        self.y_prob = {}
+        self.c_prob = None
+
+    def train(self):
+        if self.kernel == 'tfidf':
+            self.feature = self.calc_tfidf()
+        else:
+            self.feature = self.calc_bow()
+
+        for tag in self.tags:
+            self.y_prob[tag] = float(self.tags[tag]) / self.total
+
+        self.c_prob = np.zeros([len(self.tags), len(self.word2idx)])
+        Z = np.zeros([len(self.tags), 1])
+        for docidx in range(len(self.docs)):
+            tid = self.tags.keys().index(self.docs[docidx][0])
+            self.c_prob[tid] += self.feature[docidx]
+            Z[tid] = np.sum(self.c_prob[tid])
+        self.c_prob /= Z 
+
+    def predict(self, sent):
+        words = self.tokenizer(sent)
+        vec = self.get_vec(words)
+        ret, max_score = None, -1.0
+        for y, pc in zip(self.y_prob, self.c_prob):
+            score = np.sum(vec * pc * self.y_prob[y]) # p(x1....xn|yi)p(yi)
+            if score > max_score:
+                max_score = score
+                ret = y
+        return ret, 1 - max_score
+
+if __name__ == '__main__':
+    trainSet [("pos", "what are you thinking"),
+              ("pos", "How are you feeling"),
+              ("pos", "What have done to each other"),
+              ("pos", "Whah will do?")
+              ]
+    nb = NBayes(trainSet)        
    
    
 
