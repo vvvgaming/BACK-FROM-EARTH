ad9240a4e89df54944fee0ca03cf9dd1d21a0b5d
Will <willmercedes@gmail.com>
2019-02-21 11:39:17 +0800

    Imageworks
    Lightweight 18

--- Bayes/tfidf_nbayes.py
+++ Bayes/tfidf_nbayes.py
@@ -100,7 +100,7 @@ if __name__ == '__main__':
               ]
     nb = NBayes(trainSet)
     nb.train()
-    print(nb.predict("When t"))        
+    print(nb.predict("When tw"))        
    
    
 
