295aadb2b51848585cd193bb688b7e1565ad900d
Will <willmercedes@gmail.com>
2019-02-23 07:42:34 +0800

    Imageworks
    Lightweight 06

--- Structure/Division Stock.sql
+++ Structure/Division Stock.sql
@@ -6,4 +6,4 @@ SELECT
   t.customer_id AS '客户编号',
   t.customer_name AS '客户姓名',
   AES_DECRYPT (UNHEX(tu.username),'CXSOKJTSQSAZCVGHGHVDSDCG') AS '手机号'
- DATE_FORMAT(tu.create_time, '%Y-%m-%d %h:%i:%s')
+ DATE_FORMAT(tu.create_time, '%Y-%m-%d %h:%i:%s') AS 
