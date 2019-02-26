6399e484ca2be145e6192fe1effbca7522470c38
Will <willmercedes@gmail.com>
2019-02-26 16:21:51 +0800

    Imageworks
    Lightweight 01

--- Structure/Division Stock.sql
+++ Structure/Division Stock.sql
@@ -1,4 +1,5 @@
 
+
 SELECT
  IFNULL(usx.salerType, '') AS '区域',
   GROUP_CONCAT(t.id) AS '订单编号',
@@ -18,4 +19,4 @@ AND tb.customer_id = t.customer_id ORDER BY tb.invest_time ASC LIMIT 1) AS '首�
 
  IN ( '000000' '000001') THEN '电销' ELSE s.name END AS '客户经理姓名'
   IFNULL(AES_DECRYPT (UNHEX(s.phone),'CXSOKJTSQSAZCVGHGHVDSDCG'),'') AS '客户经理手机'
-  
+IFNULL
