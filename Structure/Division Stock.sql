69f07b80933a0ad69c34bdda85804f007fda34da
Will <willmercedes@gmail.com>
2019-02-27 09:00:18 +0800

    Imageworks
    Lightweight 16

--- Structure/Division Stock.sql
+++ Structure/Division Stock.sql
@@ -24,4 +24,4 @@ IFNULL(cusx.saler_name,'') AS 'C级代理人姓名',
 IFNULL(usx.B_SalerType,'') AS 'B级代理人代码',
 IFNULL(busx.saler_name,'') AS 'B级代理人姓名',
 IFNULL(usx.A_SalerType,'') AS 'A级代理人代码',
-IFNULL(busx.saler_name,)
+IFNULL(busx.saler_name,'')
