8aac76ef359a688f352bf16e3ae4486379e1b3d4
Will <willmercedes@gmail.com>
2019-03-12 09:12:46 +0800

    Imageworks
    Lightweight 07

--- Structure/Division Stock.sql
+++ Structure/Division Stock.sql
@@ -44,4 +44,5 @@ IFNULL(busx.saler_name,'') AS 'A级代理人姓名',
  IFNULL(CONCAT(SUBSTRING(AES_DECRYPT(UNHEX(u.username),'CXSOKJTSQSAZCVGHGHVDSDCG'),1,4),'***',SUBSTRING(AES_DECRYPT(UNHEX(u.username),'CXSOKJTSQSAZCVGHGHVDSDCG'),-4,4)),'') AS '推广人手机号'
 FROM
  jjjr2_sns.u_user tu JOIN jjjr2_product.tb_dealorder t ON tu.custom_id = t.customer_id
- LEFT JOIN 
+ LEFT JOIN jjjr2_sns.u_customer_relation r ON t.customer_id = r.custom_id AND t.invest_time >= r.begin_time AND t.invest_time < IFNULL(r.end_time,'9999-99-99')
+ 
