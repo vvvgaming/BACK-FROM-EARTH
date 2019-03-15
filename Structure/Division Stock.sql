c5fdee8aefb631a9ced1a44c1306d278f0a081ee
Will <willmercedes@gmail.com>
2019-03-15 10:05:35 +0800

    Imageworks
    Lightweight 12

--- Structure/Division Stock.sql
+++ Structure/Division Stock.sql
@@ -70,4 +70,4 @@ t.customer_name AS '客户姓名',
 AES_DECRYPT(UNHEX(tu.username),'CXSOKJTSQSAZCVGHGHVDSDCG') AS '手机号',
 DATE_FORMAT(tu.create_time, '%Y-%m-%d %H-%s-%s') AS '注册时间',
  (SELECT tb.invest_time FROM jjjr2_product.tb_dealorder tb
-WHERE (( tb.product_type = '100' AND tb.status IN('100','200')) OR(tb.pro)))
+WHERE (( tb.product_type = '100' AND tb.status IN('100','200')) OR(tb.prod)))
