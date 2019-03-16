4877da71190b67d6b624ffc64197ab91f0b544a6
Will <willmercedes@gmail.com>
2019-03-16 12:01:12 +0800

    Imageworks
    Lightweight 01

--- Structure/Division Stock.sql
+++ Structure/Division Stock.sql
@@ -72,3 +72,4 @@ DATE_FORMAT(tu.create_time, '%Y-%m-%d %H-%s-%s') AS '注册时间',
  (SELECT tb.invest_time FROM jjjr2_product.tb_dealorder tb
 WHERE (( tb.product_type = '100' AND tb.status IN('100','200')) OR(tb.product_type = '200' AND tb.status IN ('100', '200', '300')) OR( tb.product_type = '300' AND tb.status IN ('200','400')))
 AND tb.customer_id = t.customer_id ORDER BY tb.invest_time ASC LIMIT 1) AS '首投时间',
+ r.saler_code AS 
