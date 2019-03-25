ce421bc2d3df0f32271cf9ddf381e1fc8e360a4e
Will <willmercedes@gmail.com>
2019-03-26 06:47:09 +0800

    Imageworks
    Lightweight 03

--- Structure/Division Stock.sql
+++ Structure/Division Stock.sql
@@ -260,4 +260,6 @@ WHERE cc.customer_id = wc.customer_id AND wc.create_time >= '2019-03-25 19:00:00
 
 
 
-SELECT u.uuid,expa.city AS ,u.real_name AS '姓名' 
+SELECT 
+u.uuid AS 'id',
+expa.city AS '城市',u.real_name AS '姓名' 
