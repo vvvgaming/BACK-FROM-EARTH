01984644040f9fa7c1625a39df6fa11c307c501e
JKB <willmercedes@gmail.com>
2019-04-15 11:57:39 +0800

    Imageworks
    Lightweight 19

--- Structure/Division Stock.sql
+++ Structure/Division Stock.sql
@@ -582,7 +582,8 @@ LEFT JOIN jjjr2_sns.u_saler s ON s.saler_code = r.saler_code
 WHERE t.member_id = tu.custom_id
 AND t.trans_type IN('03','FY03')
 AND t.trans_status IN('00','10')
-AND DATE_FORMAT(t.gmt_create,'%Y-%m-%d') = DATE_FORMAT(SUBDATE(CURRENT_DATE,))
+AND DATE_FORMAT(t.gmt_create,'%Y-%m-%d') = DATE_FORMAT(SUBDATE(CURRENT_DATE,INTERVAL 1 DAY),'%Y-%m-%d');
+
 
 
 
