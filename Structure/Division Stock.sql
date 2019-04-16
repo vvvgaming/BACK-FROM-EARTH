f6d5e5ec0906e0d61ecf01300b671b4b750dc35e
JKB <willmercedes@gmail.com>
2019-04-16 08:51:55 +0800

    Imageworks
    Lightweight 19

--- Structure/Division Stock.sql
+++ Structure/Division Stock.sql
@@ -588,7 +588,42 @@ AND DATE_FORMAT(t.gmt_create,'%Y-%m-%d') = DATE_FORMAT(SUBDATE(CURRENT_DATE,INTE
 -- 到期兑付
 SELECT
 LEFT(t.serial_code) AS '交易序列编号',
-
+t.customer_id AS '客户编号',
+  t.`customer_name` AS '客户姓名',
+ CONCAT(SUBSTRING(AES_DECRYPT (UNHEX(tu.username),'CXSOKJTSQSAZCVGHGHVDSDCG'),1,4),'***',SUBSTRING(AES_DECRYPT (UNHEX(tu.username),'CXSOKJTSQSAZCVGHGHVDSDCG'),-4,4)) AS '客户手机号',
+DATE_FORMAT(tu.`create_time`,'%Y-%m-%d %H:%i:%s') AS '客户注册时间',
+  (SELECT tb.invest_time FROM jjjr2_product.`tb_dealorder` tb
+WHERE (( tb.product_type = '100' AND tb.status IN ('100','200') ) OR( tb.product_type = '200' AND tb.status IN ('100','200','300'))OR( tb.product_type = '300' AND tb.status IN ('200','400')))
+AND tb.customer_id = t.customer_id ORDER BY tb.invest_time ASC LIMIT 1) AS '首投时间',
+    r.saler_code AS '客户经理代码' ,
+   CASE WHEN s.saler_code = '000002'  THEN '公司' WHEN s.saler_code IN ( '000000','000001')  THEN '电销' ELSE s.`name` END AS '客户经理姓名',
+  AES_DECRYPT (UNHEX(s.phone),'CXSOKJTSQSAZCVGHGHVDSDCG') AS '客户经理手机号',
+  CASE s.status  WHEN '10' THEN '在职' WHEN '20' THEN '离职' ELSE s.name END AS '客户经理状态',
+  t.`product_name` AS '产品名称',
+  t.`freeze_duration` AS '产品期限',
+  t.`freeze_unit` AS '期限单位',
+ ROUND(IFNULL(t.`invest_cash`, 0),2) AS '现金投资金额',
+  ROUND( IFNULL(t.`invest_cash`*t.`freeze_duration`/12, 0),2) AS '现金年化投资金额',
+  t.`invest_time` AS '投资时间',
+  ROUND(t.`due_income`,2) AS '到期利息',
+   ROUND((t.invest_amount+t.`due_income`),2) AS '到期总金额',
+   t.end_time AS '到期时间',
+   CASE WHEN t.`status` = '100' THEN '投资中'
+  WHEN t.`status` = '200' THEN '满标' 
+  WHEN t.status = '300' THEN '赎回' END AS '订单状态',
+  u.invite_code AS '推广码',u.real_name AS '推广人姓名',
+  CONCAT(SUBSTRING(AES_DECRYPT (UNHEX(u.username),'CXSOKJTSQSAZCVGHGHVDSDCG'),1,3),'****',SUBSTRING(AES_DECRYPT (UNHEX(u.username),'CXSOKJTSQSAZCVGHGHVDSDCG'),-4,4)) AS '推广人手机号'
+FROM
+  jjjr2_sns.u_user tu JOIN jjjr2_product.`tb_dealorder` t ON tu.custom_id = t.`customer_id` 
+  LEFT JOIN jjjr2_sns.`u_customer_relation` r ON t.`customer_id` = r.`custom_id` 
+  LEFT JOIN jjjr2_sns.u_saler s ON s.`saler_code` = r.`saler_code` 
+  LEFT JOIN jjjr2_sns.`u_user` u ON tu.introducer_id = u.custom_id  
+WHERE 
+DATE_FORMAT(t.end_time,'%Y-%m')= DATE_FORMAT(CURRENT_DATE,'%Y-%m')
+AND t.`product_type` = '200' 
+AND t.`status` IN ('100','200','300')
+AND r.status = '1'
+ORDER BY t.id ASC;
 
 
 
