60d20d7b4017dc968301b7476a3f56eb6c367c31
JKB <willmercedes@gmail.com>
2019-04-28 00:17:27 +0800

    Imageworks
    Lightweight 01

--- KMeans/clusters.py
+++ KMeans/clusters.py
@@ -2,4 +2,4 @@ import matplotlib, matplotlib.pyplot as pyplot
 import pickle, pandas as pd
 import sklearn.cluster, sklearn.preprocessing
 
-alco2019 = pickle.load(open("alco2019"))
+alco2019 = pickle.load(open("alco2019."))
