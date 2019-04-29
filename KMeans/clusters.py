5e1e84a044124f0a341ecf48d415d9d20e2d626c
JKB <willmercedes@gmail.com>
2019-04-29 08:53:29 +0800

    Imageworks
    Lightweight 16

--- KMeans/clusters.py
+++ KMeans/clusters.py
@@ -5,9 +5,9 @@ import sklearn.cluster, sklearn.preprocessing
 alco2019 = pickle.load(open("alco2019.pickle", "rb"))
 
 states = pd.read_csv("states.csv", name=("States", "Standard", "Postal", "Capital"))
-colums = ["Wine", "Beer"]
+columns = ["Wine", "Beer"]
 
 kmeans = sklearn.cluster.KMeans(n_clusters=9)
-kmeans.fit(alco2019[colums])
+kmeans.fit(alco2019[columns])
 alco2019["Cluster"] = kmeans.labels_
-centers = pd.dataFrame(kmeans.cluster_centers_, )
+centers = pd.dataFrame(kmeans.cluster_centers_, columns)
