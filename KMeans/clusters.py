3141e60c0caa39d0b7d85e14b92f9f2f0b815a1b
JKB <willmercedes@gmail.com>
2019-04-29 08:54:29 +0800

    Imageworks
    Lightweight 18

--- KMeans/clusters.py
+++ KMeans/clusters.py
@@ -11,3 +11,5 @@ kmeans = sklearn.cluster.KMeans(n_clusters=9)
 kmeans.fit(alco2019[columns])
 alco2019["Cluster"] = kmeans.labels_
 centers = pd.dataFrame(kmeans.cluster_centers_, columns=columns)
+
+matplotlib.style.use
