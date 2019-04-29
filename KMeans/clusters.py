aaa078fd76f8b312c4f575f62a66dcd38e4083cc
JKB <willmercedes@gmail.com>
2019-04-29 08:49:51 +0800

    Imageworks
    Lightweight 12

--- KMeans/clusters.py
+++ KMeans/clusters.py
@@ -7,4 +7,4 @@ alco2019 = pickle.load(open("alco2019.pickle", "rb"))
 states = pd.read_csv("states.csv", name=("States", "Standard", "Postal", "Capital"))
 colums = ["Wine", "Beer"]
 
-kmeans
+kmeans = 
