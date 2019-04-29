29a624f57e9e1eea0ed638de66d06612439763fc
JKB <willmercedes@gmail.com>
2019-04-29 08:48:12 +0800

    Imageworks
    Lightweight 08

--- KMeans/clusters.py
+++ KMeans/clusters.py
@@ -4,4 +4,4 @@ import sklearn.cluster, sklearn.preprocessing
 
 alco2019 = pickle.load(open("alco2019.pickle", "rb"))
 
-states = pd.read_csv("states.csv", name=("States", "Standard", ""))
+states = pd.read_csv("states.csv", name=("States", "Standard", "Postal", ""))
