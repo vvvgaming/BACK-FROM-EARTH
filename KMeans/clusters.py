4ac6b5bd7d8c23370027dfe7309d4ab83d9fc235
JKB <willmercedes@gmail.com>
2019-05-09 07:39:26 +0800

    Imageworks
    Lightweight 16

--- KMeans/clusters.py
+++ KMeans/clusters.py
@@ -63,4 +63,4 @@ def kmeans(k,datapoints):
     
     #重点测试
     for cent in range(k):
-        ptsInClust = dataSet[nonzero(clusterAssment)]
+        ptsInClust = dataSet[nonzero(clusterAssment[:,0])]
