7b3b7cd65fbaa5a11ccfe7d93bcc3a06554d16a9
JKB <willmercedes@gmail.com>
2019-05-11 09:51:57 +0800

    Imageworks
    Lightweight 06

--- KMeans/clusters.py
+++ KMeans/clusters.py
@@ -94,7 +94,7 @@ def kmeans(k,datapoints):
                     new_center[j] = new_center[j] / float(members)
 
                 else:
-                    new_c
+                    new_ce
             
 
 
