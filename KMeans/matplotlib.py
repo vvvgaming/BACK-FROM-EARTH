cf84495b2468ff5287c556e1130dc608724d8185
JKB <willmercedes@gmail.com>
2019-05-17 05:45:15 +0800

    Imageworks
    Lightweight 08

--- KMeans/matplotlib.py
+++ KMeans/matplotlib.py
@@ -2,4 +2,4 @@ import numpy as ny
 import matplotlib.pyplot as plt
 plt.rcParams['font.sans-serif'] = 'SimHei'
 plt.rcParams['axes.unicode_minus'] = False
-data = np.load('.../data/测试数据.np')
+data = np.load('.../data/测试数据.npz')
