85fa9834078c1af50a29a49e62d76ccea7e35ed5
JKB <willmercedes@gmail.com>
2019-05-22 00:13:16 +0800

    Imageworks
    Lightweight 19

--- KMeans/matplotlib.py
+++ KMeans/matplotlib.py
@@ -25,3 +25,6 @@ plt.figure(figsize=(6,6))
 lable=['第一产业','第二产业','第三产业']
 explode = [0.01,0.01,0.01]
 plt.pie(values[-1,3:6],explode=explode,labels=label,autopct='%1.1f%%')
+plt.title('一季度国民生产总值')
+plt.savefig('.../tmp/一季度国民生产总值.png')
+plt.show()
