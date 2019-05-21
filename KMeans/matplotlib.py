3d08ed02188e6b7422af037ef3de58c8d22d0833
JKB <willmercedes@gmail.com>
2019-05-22 00:12:31 +0800

    Imageworks
    Lightweight 18

--- KMeans/matplotlib.py
+++ KMeans/matplotlib.py
@@ -24,4 +24,4 @@ frame=False, hold=None, data=None)
 plt.figure(figsize=(6,6))
 lable=['第一产业','第二产业','第三产业']
 explode = [0.01,0.01,0.01]
-plt.pie(values[-1,3:6],explode=explode,labels=label,)
+plt.pie(values[-1,3:6],explode=explode,labels=label,autopct='%1.1f%%')
