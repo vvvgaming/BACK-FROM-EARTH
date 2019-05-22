d9dd1675aa90d82d58f812091128b51569e71355
JKB <willmercedes@gmail.com>
2019-05-23 06:02:44 +0800

    Imageworks
    Lightweight 18

--- KMeans/pandas.py
+++ KMeans/pandas.py
@@ -1,4 +1,4 @@
 pandas.read_sql_table(table_name,con,schema=None,index_col=None,coerce_float=True,
 columns=None)
 pandas.read_sql_Query(sql, con, index_col=None, coerce_float=True)
-pandas.read_sql(sql, )
+pandas.read_sql(sql, con, index_col=None)
