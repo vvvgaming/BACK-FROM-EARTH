0632678a04b865207e8d4d8d56055d7978740d6f
JKB <willmercedes@gmail.com>
2019-05-25 07:07:26 +0800

    Imageworks
    Lightweight 19

--- KMeans/pandas.py
+++ KMeans/pandas.py
@@ -4,4 +4,4 @@ pandas.read_sql_Query(sql, con, index_col=None, coerce_float=True)
 pandas.read_sql(sql, con, index_col=None, coerce_float=True, columns=None)
 
 import pandas as pd
-formlist = pd.read_sql_query('show table', con = )
+formlist = pd.read_sql_query('show table', con = engine)
