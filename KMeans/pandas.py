7443ad2655cd879ce279c92c4900e9247d34ef12
JKB <willmercedes@gmail.com>
2019-05-31 00:51:59 +0800

    Imageworks
    Lightweight 17

--- KMeans/pandas.py
+++ KMeans/pandas.py
@@ -23,7 +23,6 @@ qn = pd.DataFrame(columns=('BIN', 'Boro Code', 'Boro', 'House Number', 'Street N
 
 
 df = df.T
-
 pd.set_option('chained_assignment', None)
 
 geoCodeCheck = geoCodeCheck[geoCode['Street Name'] == 'knickerbocker avenue']
