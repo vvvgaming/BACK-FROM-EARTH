e56047ccece982405d7c8bcc44208737a8d67541
JKB <willmercedes@gmail.com>
2019-06-15 05:05:34 +0800

    Imageworks
    Lightweight 10

--- KMeans/pandas.py
+++ KMeans/pandas.py
@@ -35,4 +35,4 @@ df = df[df['Name'].isin(nameList)]
 dpsub = dp[~dp['BIN'].isin(binList)]
 
 df2 = df[df['INSPECTION_ID'] == df["INSPECTION_ID.1"]]
-only_gold = df[df['Gold'] > ]
+only_gold = df[df['Gold'] > 0]
