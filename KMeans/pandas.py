16acff2c20f3db0c920278100e201f4141548199
JKB <willmercedes@gmail.com>
2019-09-21 07:27:43 +0800

    Imageworks
    Lightweight 15

--- KMeans/pandas.py
+++ KMeans/pandas.py
@@ -102,4 +102,4 @@ all_data = all_data.append(df, ignore_index = False)
 location_df = df['h_no'].apply(lambda x:pd.Series(x.split(',')))
 common_cols = list(set(df14.Address) & set(dfmn.Address))
 
-dfmn["Corner"]
+dfmn["Corner"] 
