7489c270162004500e5a0adbf2cdf62121d7f709
JKB <willmercedes@gmail.com>
2019-09-29 08:27:46 +0800

    Imageworks
    Lightweight 16

--- KMeans/pandas.py
+++ KMeans/pandas.py
@@ -106,4 +106,4 @@ dfmn["Corner"] = 0
 for i in range(0, len(dfmn))
 
 lv['Correct Duration Sum'] = lv['Correct Duration'].groupby(v['CityTime ID']).transform('sum')
-qnG = qn.groupby(['BIN','Year']).sum
+qnG = qn.groupby(['BIN','Year']).sum()
