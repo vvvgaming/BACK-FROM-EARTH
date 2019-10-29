51e54819f563000312d15dd159246b7ed333cdee
JKB <willmercedes@gmail.com>
2019-10-30 00:21:57 +0800

    Imageworks
    Lightweight 13

--- KMeans/pandas.py
+++ KMeans/pandas.py
@@ -133,4 +133,4 @@ df['Inspection Time'] = df["Inspection Time"].apply(lamda d : d.time())
 df['Difference'] = (df["hearing_date"] - df["ticket_issued_date"]).dt.days
 
 d = df2["Variance End Date"][j] - df2["Variance Start Date"][j]
-diff = d.day
+diff = d.days
