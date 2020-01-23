d671ddbfcf75949b2c13df8016f754ad959adb90
JKB <willmercedes@gmail.com>
2020-01-23 10:47:27 +0800

    Imageworks
    Lightweight 08

--- KMeans/pandas.py
+++ KMeans/pandas.py
@@ -199,4 +199,4 @@ print data.apply(num_missing, axis = 0)#axis = 0 defines that function is to be 
 
 #Applying per row:
 print "\nMissing values per row:"
-print data.apply(num_missing, axis = 1).head() #axis = 1 defines that functi
+print data.apply(num_missing, axis = 1).head() #axis = 1 defines that functio
