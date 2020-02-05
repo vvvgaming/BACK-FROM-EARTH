b7c9146e595ddfb20563b0f7974f6c5265ee9a25
JKB <willmercedes@gmail.com>
2020-02-06 01:21:02 +0800

    Imageworks
    Lightweight 17

--- KMeans/pandas.py
+++ KMeans/pandas.py
@@ -209,4 +209,4 @@ mode(data['Gender']).mode[0]
 
 #Impute the values:
 data['Gender'].fillna(mode(data['Gender']).mode[0], inplace = True)
-data['Married'].fillna(mode(data['Married']).mo)
+data['Married'].fillna(mode(data['Married']).mod)
