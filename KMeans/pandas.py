38b8553b61f1d0813c5bb4095ac811ffe8233f0b
JKB <willmercedes@gmail.com>
2020-02-14 00:22:19 +0800

    Imageworks
    LIghtweight 05

--- KMeans/pandas.py
+++ KMeans/pandas.py
@@ -212,4 +212,4 @@ data['Gender'].fillna(mode(data['Gender']).mode[0], inplace = True)
 data['Married'].fillna(mode(data['Married']).mode[0], inplace = True)
 data['Self_Emploed'].fillna(mode(data['Self_Emploed']).mode[0], inplace = True)
 
-#Now check the #missing values again to confirm
+#Now check the #missing values again to confirm:
