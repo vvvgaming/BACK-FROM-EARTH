70633b628cd2e6f6e9b8214af8b45de02a5e6385
JKB <willmercedes@gmail.com>
2020-02-22 00:01:28 +0800

    Imageworks
    Lightweight 004

--- KMeans/pandas.py
+++ KMeans/pandas.py
@@ -216,4 +216,4 @@ data['Self_Emploed'].fillna(mode(data['Self_Emploed']).mode[0], inplace = True)
 print data.apply(num_missing, axis = 0)
 
 #Determine pivot table
-imput_grps = data.pivot_table(values = ["LoanAmount"], index = ["Gender", "Married", "Self"])
+imput_grps = data.pivot_table(values = ["LoanAmount"], index = ["Gender", "Married", "Self_"])
