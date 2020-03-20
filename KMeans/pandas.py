e0ec7ba7010e75e325a953369b6af26f72fcbafd
JKB <willmercedes@gmail.com>
2020-03-21 00:06:22 +0800

    Imageworks
    Lightweight 001

--- KMeans/pandas.py
+++ KMeans/pandas.py
@@ -239,3 +239,4 @@ data_merged = data.merge(right=prop_rates, how='inner',left_on='Property_Area',r
 data_merged.pivot_table(values='Credit_History',index=['Property_Area','rates'], aggfunc=len)
 
 
+d
