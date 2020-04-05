7c8cbb91f47ffb3dab3d8ac1e0f861dfe523ad42
JKB <willmercedes@gmail.com>
2020-04-05 22:22:39 +0800

    Imageworks
    Lightweight 008

--- KMeans/pandas.py
+++ KMeans/pandas.py
@@ -307,6 +307,6 @@ for i, row in colTypes.iterrows():  #i: dataframe index; row: each row in series
 print data.dtypes
 
 
-select hzs.u ID_Number, sum(hns.amount) delay_amount
+select hzs.c ID_Number, sum(hns.amount) delay_amount
 from
 ((select u.cert))
