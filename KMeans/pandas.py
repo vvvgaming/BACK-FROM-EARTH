75e43f3e1e678a4445104a065df54a5d0facec52
JKB <willmercedes@gmail.com>
2020-04-12 00:20:16 +0800

    Imageworks
    Lightweight 019

--- KMeans/pandas.py
+++ KMeans/pandas.py
@@ -310,5 +310,5 @@ print data.dtypes
 select hzs.cert_no ID_Number, sum(hns.amount) delay_amount
 from
 ((select u.cert_no ID_Number, sum(t.amount) invest_amount from shanba_deposit.v_users u
-inner join shanba_deposit.v_e_accounts e on u.
+inner join shanba_deposit.v_e_accounts e on u.i
 ))
