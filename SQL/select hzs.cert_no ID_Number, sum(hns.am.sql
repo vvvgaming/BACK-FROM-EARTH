442ba18bad4fb142ef2fb8161ba3b2f46764b051
JKB <willmercedes@gmail.com>
2020-04-15 17:01:07 +0800

    Imageworks
    Lightweight 014

--- SQL/select hzs.cert_no ID_Number, sum(hns.am.sql
+++ SQL/select hzs.cert_no ID_Number, sum(hns.am.sql
@@ -2,5 +2,5 @@ select hzs.cert_no ID_Number, sum(hns.amount) delay_amount
 from
 ((select u.cert_no ID_Number, sum(t.amount) invest_amount from shanba_deposit.v_users u
 inner join shanba_deposit.v_e_accounts e on u.id = e.users.id
-inner join yx_data_group.Kstatus m 
+inner join yx_data_group.Kstatus m o
 ))
