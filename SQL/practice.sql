a6245acce125980206e3eaeedc152d6f8714f831
JKB <willmercedes@gmail.com>
2020-04-17 18:42:32 +0800

    Imageworks
    Lightweight 006

--- SQL/practice.sql
+++ SQL/practice.sql
@@ -3,5 +3,5 @@ from
 ((select u.cert_no ID_Number, sum(t.amount) invest_amount from shanba_deposit.v_users u
 inner join shanba_deposit.v_e_accounts e on u.id = e.users.id
 inner join yx_data_group.Kstatus m on e.merchant_id = m.merchant
-
+i
 ))
