17b48e7c9f28711d5c38bf47f7a39010be66a0df
JKB <willmercedes@gmail.com>
2020-07-02 08:51:36 +0800

    Imageworks
    Lightweight 011

--- IDS2501/Multinomial Logistic Regression.py
+++ IDS2501/Multinomial Logistic Regression.py
@@ -2,6 +2,6 @@ import numpy as np
 import pandas as pd
 from datetime import datetime, date, timedelta
 from scipy.stats import skew
-from scipy.special import bo
+from scipy.special import box
 from scipy.
 from sklearn.
