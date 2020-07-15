5b7d3cb029b5b2e291e3dbc42fdded77feab63ec
JKB <willmercedes@gmail.com>
2020-07-15 08:14:46 +0800

    Imageworks
    Lightweight 018

--- IDS2501/prophet.py
+++ IDS2501/prophet.py
@@ -2,5 +2,5 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from fbprophet import Prophet
-from fbprophet.diagnostics import cross.validation
+from fbprophet.diagnostics import cross_validation
 from fbprophet.diagnostics import performance
