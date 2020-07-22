59c3f9446ebd53c979e3cc7249558eb510d4e46f
JKB <willmercedes@gmail.com>
2020-07-22 08:24:18 +0800

    Imageworks
    Lightweight 016

--- IDS2501/prophet.py
+++ IDS2501/prophet.py
@@ -8,4 +8,4 @@ from fbprophet.plot import plot_cross_validation_metrics
 import warnings
 warnings.filterwarnings('ignore')
 
-data_user = pd.read_csv('')
+data_user = pd.read_csv('user_balance_table.csv')
