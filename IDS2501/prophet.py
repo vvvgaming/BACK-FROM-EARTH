752e46619067a1d9d5d9c30d533c64ff1f1c2e82
JKB <willmercedes@gmail.com>
2020-08-06 07:15:04 +0800

    Imageworks
    Lightweight 014

--- IDS2501/prophet.py
+++ IDS2501/prophet.py
@@ -25,4 +25,4 @@ def FB(data: pd.DataFrame) -> pd.DataFrame:
         'y':data.total_purchase_amt,
     })
 
-    df['cap'] = data.total_purchase_amt.values.ma
+    df['cap'] = data.total_purchase_amt.values.max
