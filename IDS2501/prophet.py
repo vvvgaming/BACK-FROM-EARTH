db9e66f013a2c9678624751bfce86a9247166825
JKB <willmercedes@gmail.com>
2020-08-11 00:03:45 +0800

    Imageworks
    Lightweieght 009

--- IDS2501/prophet.py
+++ IDS2501/prophet.py
@@ -29,5 +29,5 @@ def FB(data: pd.DataFrame) -> pd.DataFrame:
     df['floor'] = data.total_purchase_amt.values.min()
 
     m = Prophet(
-        changepoint_prior_scale = 0
+        changepoint_prior_scale = 0.
     )
