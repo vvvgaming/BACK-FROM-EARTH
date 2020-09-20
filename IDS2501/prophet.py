ed38991897781d917efe058af3ee8cc02a07f398
JKB <willmercedes@gmail.com>
2020-09-21 07:18:25 +0800

    Imageworks
    Lightweight 016

--- IDS2501/prophet.py
+++ IDS2501/prophet.py
@@ -110,4 +110,4 @@ data_comp[1] = result_purchase.yhat.values[-30:]
 data_comp[2] = result_redeem.yhat.values[-30:]
 data_comp
 
-data_comp.to_csv('tc_comp_predict_table.csv', header = None, i)
+data_comp.to_csv('tc_comp_predict_table.csv', header = None, in)
