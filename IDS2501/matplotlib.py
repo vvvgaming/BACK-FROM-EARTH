dbd243c4faa9a932166f3d65f917313226d519ba
JKB <willmercedes@gmail.com>
2020-10-04 09:27:26 +0800

    Imageworks
    Lightweight 019

--- IDS2501/matplotlib.py
+++ IDS2501/matplotlib.py
@@ -34,4 +34,4 @@ minimumSpainningTree(graph):
     mark all vertices and edges as unvisited
     mark some vertex, say v,  as visited
     for all the vertices:
-        find the least weight edge from a 
+        find the least weight edge from a v
