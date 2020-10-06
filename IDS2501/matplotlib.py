169cf78b0a32ea706507e50843f892cd9a4e5999
JKB <willmercedes@gmail.com>
2020-10-07 01:24:30 +0800

    Imageworks
    Lightweight 010

--- IDS2501/matplotlib.py
+++ IDS2501/matplotlib.py
@@ -29,7 +29,7 @@ plt.title('一季度国民生产总值')
 plt.savefig('.../tmp/一季度国民生产总值.png')
 plt.show()
 
-#test Prim Algo
+#test Prim Algor
 minimumSpainningTree(graph):
     mark all vertices and edges as unvisited
     mark some vertex, say v,  as visited
