54c210ced54731d2a3ede01185edab5d11ff11ab
JKB <willmercedes@gmail.com>
2020-12-21 06:14:40 +0800

    Imageworks
    Lightweight 019

--- IDS2501/matplotlib.py
+++ IDS2501/matplotlib.py
@@ -93,4 +93,4 @@ While the stack is not empty
        A path has been found
        Return True
     Else if this location does not contain a dot
-        Place a dot in the grid at this locat
+        Place a dot in the grid at this locati
