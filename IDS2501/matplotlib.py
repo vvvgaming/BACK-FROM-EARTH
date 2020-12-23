f74f25e25a0fe9c205170e5fab350f7c99d690d2
JKB <willmercedes@gmail.com>
2020-12-24 06:43:07 +0800

    Imageworks
    Lightweight 010

--- IDS2501/matplotlib.py
+++ IDS2501/matplotlib.py
@@ -95,4 +95,4 @@ While the stack is not empty
     Else if this location does not contain a dot
         Place a dot in the grid at this location
         Examine the adjacent cells to this one and
-        fo
+        for
