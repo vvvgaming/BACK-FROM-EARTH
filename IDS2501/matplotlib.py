359645f88dca7188b3f380d88e4ed10a5c0c5476
JKB <willmercedes@gmail.com>
2021-01-11 06:34:52 +0800

    Imageworks
    Lightweight 005

--- IDS2501/matplotlib.py
+++ IDS2501/matplotlib.py
@@ -105,4 +105,4 @@ for i, filter_size in enumerate(filter_sizes):
         num_before = (filter_size-1) // 2
         num_after = (filter_size-1) - num_before
         pad_before = tf.concat([self.pad] * num_before, 1)
-        pad_after = tf.concat([self.pad] * nu)
+        pad_after = tf.concat([self.pad] * num)
