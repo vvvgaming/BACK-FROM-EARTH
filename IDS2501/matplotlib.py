6eafd79ef517fe7712363f17f97461c927ff70c0
JKBRush <willmercedes@gmail.com>
2021-04-30 09:42:31 +0800

    Imageworks
    Lightweight 021

--- IDS2501/matplotlib.py
+++ IDS2501/matplotlib.py
@@ -170,4 +170,4 @@ def TextProcessing(folder_path, test_size = 0.2):
     class_list = []
 
     for folder in folder_list:
-        new_folder_path = os.path.jo
+        new_folder_path = os.path.joi
