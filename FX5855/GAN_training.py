807ba1385eb0ed1fa4218b8f3fadcae0ce2c8575
IamReefShark <zhijianliu@pm.me>
2021-10-12 05:45:43 +0800

    Imageworks
    Lightweight 014

--- FX5855/GAN_training.py
+++ FX5855/GAN_training.py
@@ -27,5 +27,5 @@ def Adversarial_training(Alice, Eve, Bob, FFN, train_data_loader, batch_size, FF
 		  	image = train_data[0]
 		  	keys = train_data[0][arr]
 		  	Alice_vec = Alice.extract_latent(image)
-		  	key_vec = Al
+		  	key_vec = Ali
 		  	_,x,y,z =
