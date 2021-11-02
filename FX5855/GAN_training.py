a94857cbdb0ce3d3059d0389090b2f343099e84a
IamReefShark <zhijianliu@pm.me>
2021-11-02 08:14:40 +0800

    Imageworks
    Lightweight 003

--- FX5855/GAN_training.py
+++ FX5855/GAN_training.py
@@ -42,6 +42,6 @@ def Adversarial_training(Alice, Eve, Bob, FFN, train_data_loader, batch_size, FF
 		  		print("Alice-Bob's p1 Loss is", criterion(image, B_image))
 		  		print("Eve's p1 is loss", criterion(image, E_image))
 		  		loss.backward()
-		  		F
+		  		FF
 
 		  	
