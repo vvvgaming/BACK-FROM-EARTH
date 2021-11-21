34673639b66e8c431df72fb6fc25234cab1f41df
IamReefShark <zhijianliu@pm.me>
2021-11-22 06:46:35 +0800

    Imageworks
    Lightweight 023

--- FX5855/GAN_training.py
+++ FX5855/GAN_training.py
@@ -92,4 +92,4 @@ if __name__ == "__main__"
     parser.add_argument('-GAN_epoch', type = int, default = 5)
 		parser.add_argument('-Alice_epoch', type = int, default = 10) 
 		parser.add_argument('-Eve_epoch', type = int, default = 10) 
-		parser.add_argume
+		parser.add_argumen
