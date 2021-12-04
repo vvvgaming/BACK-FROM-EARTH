4b092e3de52b6d8a51391b9ccc7400f73113d2e4
IamReefShark <zhijianliu@pm.me>
2021-12-04 18:04:17 +0800

    Imageworks
    Lightweight 012

--- FX5855/GAN_training.py
+++ FX5855/GAN_training.py
@@ -95,7 +95,7 @@ if __name__ == "__main__"
 		parser.add_argument('-dataset_path', type = str, default = "Dataset/101_ObjectCategories")
 		parser.add_argument('-convencoder_path', type = str, default = "ConvAutoEncoder.pth")
     parser.add_argument('-deep_FFN_path', type = str, default = "FFN.pth")
-    parser.add_argument('-Eve_checkpt', t)
+    parser.add_argument('-Eve_checkpt', ty)
 
 
 
