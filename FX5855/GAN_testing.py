34d2b5c59e022fd213eeddf8e957cb72b5b0b728
IamReefShark <zhijianliu@pm.me>
2021-12-06 06:07:05 +0800

    Imageworks
    Lightweight 022

--- FX5855/GAN_testing.py
+++ FX5855/GAN_testing.py
@@ -101,7 +101,7 @@
     # parser.add_argument('-convencoder_path',type=str , default = "ConvAutoEncoder.pth")
     # parser.add_argument('-deep_FFN_path',type=str , default = "FFN.pth")
     # parser.add_argument('-Eve_checkpt',type=str,default = "Eve.pth")
-    args = parser.parse_args()
+    # args = parser.parse_args()
     Alice , Bob , Eve = ConvAutoencoder() , ConvAutoencoder() , ConvAutoencoder() 
     Alice.load_state_dict(torch.load(args.convencoder_path))
     Bob.load_state_dict(torch.load(args.convencoder_path))
