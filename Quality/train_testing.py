1dbfe9b5a84a344c051d352defcecab4c96a5e55
IamReefShark <zhijianliu@pm.me>
2022-01-12 07:52:48 +0800

    Pure Ambition
    Pure aggressive 020

--- Quality/train_testing.py
+++ Quality/train_testing.py
@@ -2,7 +2,7 @@
 # import numpy as np
 # from torchvision import datasets
 # from torchvision import transforms
-from torch.utils.data.sampler import SubsetRandomSampler
+# from torch.utils.data.sampler import SubsetRandomSampler
 from torch import nn as nn 
 import torch.nn.functional as F
 import argparse
