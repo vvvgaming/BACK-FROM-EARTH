12481bc33df18cb0c31af544a7451a3e40152a92
IamReefShark <zhijianliu@pm.me>
2022-01-26 00:07:10 +0800

    Pure Ambition
    Pure aggressive 027

--- Quality/train_testing.py
+++ Quality/train_testing.py
@@ -15,7 +15,7 @@
         # self.conv1 = nn.Conv2d(3, 16, 2, padding=1)  
         # self.conv2 = nn.Conv2d(16, 4, 2, padding=1)
         # self.pool = nn.MaxPool2d(2, 2)
-        self.t_conv1 = nn.ConvTranspose2d(4, 16, 2, stride=2)
+        # self.t_conv1 = nn.ConvTranspose2d(4, 16, 2, stride=2)
         self.t_conv2 = nn.ConvTranspose2d(16, 3, 2, stride=2)
 
     def forward(self, x):
