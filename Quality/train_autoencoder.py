b509a2a21f177c924febcf86721ebf24a2f62bfa
IamReefShark <zhijianliu@pm.me>
2022-01-28 01:19:36 +0800

    Pure Ambition
    Pure aggressive 003

--- Quality/train_autoencoder.py
+++ Quality/train_autoencoder.py
@@ -15,4 +15,4 @@ class ConvAutoencoder(nn.Module):
 		self.conv2 = nn.Conv2d(16, 4, 2, padding = 1)
 		self.pool = nn.MaxPool2d(2, 2)
 		self.t_conv1 = nn.ConvTranspose2d(4, 16, 2, stride = 2)
-		self.t_conv2 = nn.ConvTranspos
+		self.t_conv2 = nn.ConvTranspose
