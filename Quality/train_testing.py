15f65a2a6aed3343d20b91fe69c0b69d78dab910
IamReefShark <zhijianliu@pm.me>
2022-02-01 18:17:59 +0800

    Pure Ambition
    Pure aggressive 022

--- Quality/train_testing.py
+++ Quality/train_testing.py
@@ -22,7 +22,7 @@
     #     x = F.relu(self.conv1(x))
         # x = self.pool(x)
         # x = F.relu(self.conv2(x))
-        x = self.pool(x)  
+        # x = self.pool(x)  
         x = F.relu(self.t_conv1(x))
         x = torch.tanh(self.t_conv2(x))
         return x
