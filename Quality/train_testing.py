6139ca1b27923bc127548ff795a31dcb30f51e1d
IamReefShark <zhijianliu@pm.me>
2022-02-03 00:06:31 +0800

    Pure Ambition
    Pure aggressive 024

--- Quality/train_testing.py
+++ Quality/train_testing.py
@@ -24,7 +24,7 @@
         # x = F.relu(self.conv2(x))
         # x = self.pool(x)  
         # x = F.relu(self.t_conv1(x))
-        x = torch.tanh(self.t_conv2(x))
+        # x = torch.tanh(self.t_conv2(x))
         return x
       
     def extract_latent(self,x):
