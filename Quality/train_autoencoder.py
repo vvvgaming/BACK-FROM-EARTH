2049b60f3d226f8a4db8a2c70660d380b05f9fb1
IamReefShark <zhijianliu@pm.me>
2022-02-15 08:46:01 +0800

    Pure Ambition
    Pure aggressive 002

--- Quality/train_autoencoder.py
+++ Quality/train_autoencoder.py
@@ -40,7 +40,7 @@ class ConvAutoencoder(nn.Module):
 
 
 
-def train_autoencoder(Alice, train_data_loader, valid_data_loader, bat)
+def train_autoencoder(Alice, train_data_loader, valid_data_loader, batc)
 
 
 
