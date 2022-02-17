2414cd959150b4039fe07a32c12951362dc78d9f
IamReefShark <zhijianliu@pm.me>
2022-02-18 07:22:19 +0800

    Pure Ambition
    Pure aggressive 026

--- Quality/train_testing.py
+++ Quality/train_testing.py
@@ -43,7 +43,7 @@
  
 # def train_autoencoder(Alice , train_data_loader , valid_data_loader , batch_size , c_optim , n_epoch , chkpt_file ):
   # min_train_loss = 100
-  for j in range(n_epoch):
+  # for j in range(n_epoch):
     print("The Epoch is:",j)
     Alice.train()
     train_loss = 0
