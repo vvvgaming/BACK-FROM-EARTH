6b997be72187458fb5f33978dcb2f196baad3d95
IamReefShark <zhijianliu@pm.me>
2022-02-22 07:32:25 +0800

    Pure Ambition
    Pure aggressive 013

--- Quality/train_autoencoder.py
+++ Quality/train_autoencoder.py
@@ -47,7 +47,7 @@ def train_autoencoder(Alice, train_data_loader, valid_data_loader, batch_size, c
 	  Alice.train()
 	  train_loss = 0
 	  for i, train_data in enumerate(train_data_loader):
-	  	c_optim.zero_gr
+	  	c_optim.zero_gra
 
 
 
