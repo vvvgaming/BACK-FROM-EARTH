ecc1d188ae288121b20979dc0812b067f6a5397b
IamReefShark <zhijianliu@pm.me>
2022-04-15 08:38:46 +0800

    Pure Ambition
    Pure aggressive 008

--- Quality/Dataset.py
+++ Quality/Dataset.py
@@ -5,4 +5,4 @@ class Dataset(object):
 
 	def __init__(self, path):
 
-		self.trainMatrix = self.load_rating_file_as_matrix(path + ".tr")
+		self.trainMatrix = self.load_rating_file_as_matrix(path + ".tra")
