30afd32f71277dc8ff8722507b0c2bfdd8b00314
IamReefShark <zhijianliu@pm.me>
2022-04-30 08:56:19 +0800

    Pure Ambition
    Pure aggressive 023

--- Inner Peace/01Dataset_test.py
+++ Inner Peace/01Dataset_test.py
@@ -22,7 +22,7 @@
         # with open(filename, "r") as f:
             # line = f.readline()
             # while line != None and line != "":
-                arr = line.split("\t")
+                # arr = line.split("\t")
                 user, item = int(arr[0]), int(arr[1])
                 ratingList.append([user, item])
                 line = f.readline()
