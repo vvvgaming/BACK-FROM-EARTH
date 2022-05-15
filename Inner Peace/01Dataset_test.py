9a48cbe9ca666cb19b5ce66833ff937b984ae058
IamReefShark <zhijianliu@pm.me>
2022-05-15 10:09:58 +0800

    Pure Ambition
    Pure aggressive 001

--- Inner Peace/01Dataset_test.py
+++ Inner Peace/01Dataset_test.py
@@ -39,7 +39,7 @@
                     # negatives.append(int(x))
                 # negativeList.append(negatives)
                 # line = f.readline()
-        return negativeList
+        # return negativeList
     
     def load_rating_file_as_matrix(self, filename):
         num_users, num_items = 0, 0
