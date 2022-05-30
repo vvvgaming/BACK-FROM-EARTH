e99ea133a34a5ff813c1092da4dd5d43e55641c6
IamReefShark <zhijianliu@pm.me>
2022-05-31 07:33:26 +0800

    Pure Ambition
    Pure aggressive 021

--- Inner Peace/01Dataset_test.py
+++ Inner Peace/01Dataset_test.py
@@ -56,7 +56,7 @@
         # with open(filename, "r") as f:
             # line = f.readline()
             # while line != None and line != "":
-                arr = line.split("\t")
+                # arr = line.split("\t")
                 user, item, rating = int(arr[0]), int(arr[1]), float(arr[2])
                 if (rating > 0):
                     mat[user, item] = 1.0
