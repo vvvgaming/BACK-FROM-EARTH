cb4aeab3101377fa697a6eb577338cfe7ba668f1
IamReefShark <zhijianliu@pm.me>
2022-06-02 08:02:02 +0800

    Pure Ambition
    Pure aggressive 025

--- Inner Peace/01Dataset_test.py
+++ Inner Peace/01Dataset_test.py
@@ -57,7 +57,7 @@
             # line = f.readline()
             # while line != None and line != "":
                 # arr = line.split("\t")
-                user, item, rating = int(arr[0]), int(arr[1]), float(arr[2])
+                # user, item, rating = int(arr[0]), int(arr[1]), float(arr[2])
                 if (rating > 0):
                     mat[user, item] = 1.0
                 line = f.readline()    
