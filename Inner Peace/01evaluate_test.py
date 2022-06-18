ea724e5d8a80fdb3452dd92469609db48a8ddbce
IamReefShark <zhijianliu@pm.me>
2022-06-19 04:42:01 +0800

    Pure Ambition
    Pure aggressive 008

--- Inner Peace/01evaluate_test.py
+++ Inner Peace/01evaluate_test.py
@@ -21,7 +21,7 @@
     # _testNegatives = testNegatives
     # _K = K
         
-    hits, ndcgs = [],[]
+    # hits, ndcgs = [],[]
     if(num_thread > 1): # Multi-thread
         pool = multiprocessing.Pool(processes=num_thread)
         res = pool.map(eval_one_rating, range(len(_testRatings)))
