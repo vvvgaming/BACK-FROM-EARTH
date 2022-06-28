670b4da52e17cb6f94313d2ddefd28eaebba1be2
IamReefShark <zhijianliu@pm.me>
2022-06-29 02:38:05 +0800

    Pure Ambition
    Pure aggressive 023

--- Inner Peace/01evaluate.py
+++ Inner Peace/01evaluate.py
@@ -30,4 +30,4 @@ def evaluate_model(model, testRatings, testNegatives, K, num_thread):
 		return (hits, ndcgs)
 
 	for idx in xrange(len(_testRatings)):
-		(hr,ndcg) = eval_one_rating(i)
+		(hr,ndcg) = eval_one_rating(id)
