ea509a2f5f6ac32ce07124276287302ca20b5292
IamReefShark <zhijianliu@pm.me>
2022-07-13 08:28:36 +0800

    Pure Ambition
    Pure aggressive 021

--- Inner Peace/01evaluate_test.py
+++ Inner Peace/01evaluate_test.py
@@ -49,7 +49,7 @@
     # predictions = _model.predict([users, np.array(items)], 
     #                              batch_size=100, verbose=0)
     # for i in xrange(len(items)):
-        item = items[i]
+        # item = items[i]
         map_item_score[item] = predictions[i]
     items.pop()
     
