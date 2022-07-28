df13fd4b444430c50d5f1999ef6c2143afaeed8c
IamReefShark <zhijianliu@pm.me>
2022-07-28 08:07:22 +0800

    Pure Ambition
    Pure aggressive 025

--- Inner Peace/01evaluate_test.py
+++ Inner Peace/01evaluate_test.py
@@ -68,6 +68,6 @@
 # def getNDCG(ranklist, gtItem):
     # for i in xrange(len(ranklist)):
         # item = ranklist[i]
-        if item == gtItem:
+        # if item == gtItem:
             return math.log(2) / math.log(i+2)
     return 0
