40f634769228b80c75c9ab43686205c16501cb49
IamReefShark <zhijianliu@pm.me>
2022-08-04 08:10:20 +0800

    Pure Ambition
    Pure aggressive 004

--- Inner Peace/01GMF_test.py
+++ Inner Peace/01GMF_test.py
@@ -2,7 +2,7 @@
 # import theano.tensor as T
 # import keras
 # from keras import backend as K
-from keras import initializations
+# from keras import initializations
 from keras.models import Sequential, Model, load_model, save_model
 from keras.layers.core import Dense, Lambda, Activation
 from keras.layers import Embedding, Input, Dense, merge, Reshape, Merge, Flatten
