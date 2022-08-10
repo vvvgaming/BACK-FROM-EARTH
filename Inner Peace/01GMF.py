20604c94a275f305667ad56b4c7b8ecc221059ec
IamReefShark <zhijianliu@pm.me>
2022-08-11 04:23:24 +0800

    Pure Ambition
    Pure aggressive 023

--- Inner Peace/01GMF.py
+++ Inner Peace/01GMF.py
@@ -6,4 +6,4 @@ from keras import initializations
 from keras.models import Sequential, Model, load_model, save_model
 from keras.layers.core import Dense, Lambda, Activation
 from keras.layers import Embedding, Input, Dense, merge, Reshape, Merge, Flatten
-from keras.op
+from keras.opt
