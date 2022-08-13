cacc770c047664ac3d8c292d45e7e07496e90c61
IamReefShark <zhijianliu@pm.me>
2022-08-14 06:25:19 +0800

    Update 01GMF_test.py

--- Inner Peace/01GMF_test.py
+++ Inner Peace/01GMF_test.py
@@ -7,7 +7,7 @@
 # from keras.layers.core import Dense, Lambda, Activation
 # from keras.layers import Embedding, Input, Dense, merge, Reshape, Merge, Flatten
 # from keras.optimizers import Adagrad, Adam, SGD, RMSprop
-from keras.regularizers import l2
+# from keras.regularizers import l2
 from Dataset import Dataset
 from evaluate import evaluate_model
 from time import time
