a2bf58f65f626050a4499738dbb5bec765904e3e
IamReefShark <zhijianliu@pm.me>
2022-08-23 08:05:57 +0800

    Pure Ambition
    Pure aggressive 008

--- Inner Peace/01GMF_test.py
+++ Inner Peace/01GMF_test.py
@@ -18,7 +18,7 @@
 
 #################### Arguments ####################
 # def parse_args():
-    parser = argparse.ArgumentParser(description="Run GMF.")
+    # parser = argparse.ArgumentParser(description="Run GMF.")
     parser.add_argument('--path', nargs='?', default='Data/',
                         help='Input data path.')
     parser.add_argument('--dataset', nargs='?', default='ml-1m',
