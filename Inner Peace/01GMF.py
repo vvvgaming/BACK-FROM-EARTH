aeb1d6a28ffc2c5b4119d1d9fd5d52158e75937c
IamReefShark <zhijianliu@pm.me>
2022-08-25 07:09:29 +0800

    Pure Ambition
    Pure aggressive 001

--- Inner Peace/01GMF.py
+++ Inner Peace/01GMF.py
@@ -19,4 +19,4 @@ import argparse
 
 def parse_args():
 	parser = argparse.ArgumentParser(description = "Run GMF.")
-	parser.add_argument('--path', nargs = '?', defau)
+	parser.add_argument('--path', nargs = '?', defaul)
