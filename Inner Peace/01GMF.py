8b60639b6ac035e1084abefc57636aca04fada76
IamReefShark <zhijianliu@pm.me>
2022-08-31 07:19:52 +0800

    Pure Ambition
    Pure aggressive 023

--- Inner Peace/01GMF.py
+++ Inner Peace/01GMF.py
@@ -21,4 +21,4 @@ def parse_args():
 	parser = argparse.ArgumentParser(description = "Run GMF.")
 	parser.add_argument('--path', nargs = '?', default = 'Data/', help = 'Input data path.')
 	parser.add_argument('--dataset', nargs = '?', default = 'ml-1m', help = 'Choose a dataset.')
-	parser.add_argument('--epochs', type = int, default = 100, help)
+	parser.add_argument('--epochs', type = int, default = 100, help )
