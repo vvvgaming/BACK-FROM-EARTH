30f7e299e8d006bbfde21e0a3741b04f79c4198e
IamReefShark <zhijianliu@pm.me>
2022-09-13 06:35:43 +0800

    Pure Ambition
    Pure aggressive 017

--- Inner Peace/01GMF.py
+++ Inner Peace/01GMF.py
@@ -25,4 +25,4 @@ def parse_args():
 	parser.add_argument('--batch_size', type = int, default = 256, help = 'Batch size.')
 	parser.add_argument('--num_factors', type = int, default = 8, help = 'Embedding size.')
 	parser.add_argument('--regs', nargs = '?', default = '[0,0]', help = "Regularization for user and item embeddings.")
-	parser.add_argument('--num_neg', type = int, default = 4,)
+	parser.add_argument('--num_neg', type = int, default = 4, )
