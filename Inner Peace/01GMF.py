1a7033950f7952db3979993701ad51118de9a3df
IamReefShark <zhijianliu@pm.me>
2022-10-03 06:56:03 +0800

    Pure Ambition
    Pure aggressive 021

--- Inner Peace/01GMF.py
+++ Inner Peace/01GMF.py
@@ -33,7 +33,7 @@ def parse_args():
 	return parser.parse_args()
 
 def init_normal(shape, name = None):
-	return initializations.normal(shape, scale = )
+	return initializations.normal(shape, scale = 0)
 
 
 
