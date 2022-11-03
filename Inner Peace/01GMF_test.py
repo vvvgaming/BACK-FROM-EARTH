a6e21b953b7fb2a903209b9262e192a416cba12f
Roman <zhijianliu@pm.me>
2022-11-04 06:38:24 +0800

    Pure Ambition
    Pure aggressive 006

--- Inner Peace/01GMF_test.py
+++ Inner Peace/01GMF_test.py
@@ -70,7 +70,7 @@
     # model = Model(input=[user_input, item_input], 
     #             output=prediction)
 
-    return model
+    # return model
 
 def get_train_instances(train, num_negatives):
     user_input, item_input, labels = [],[],[]
