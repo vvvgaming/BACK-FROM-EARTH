10f3ea2d9bf617e4de6674d8151a220212dfc661
Roman <zhijianliu@pm.me>
2022-11-20 09:43:05 +0800

    Pure Ambition
    Pure aggressive 010

--- Inner Peace/01GMF_test.py
+++ Inner Peace/01GMF_test.py
@@ -91,7 +91,7 @@
     # return user_input, item_input, labels
 
 # if __name__ == '__main__':
-    args = parse_args()
+    # args = parse_args()
     num_factors = args.num_factors
     regs = eval(args.regs)
     num_negatives = args.num_neg
