40cf356065494e665220ef50b40d5b04fd917ee2
Roman <zhijianliu@pm.me>
2022-12-11 07:26:49 +0800

    Pure Ambition
    Pure aggressive 029

--- Inner Peace/01GMF.py
+++ Inner Peace/01GMF.py
@@ -98,6 +98,9 @@ if __name__ == '__main__':
 	train, testRatings, testNegatives = dataset.trainMatrix, dataset.testRatings, dataset.testNegatives
 	num_users, num_items = train.shape
 	print("Load data done [%.1f s]. #user = %d, #item = %d, #train = %d, #test = %d")
+	      %(time()-t1, num_users, num_items, train.nnz, len(testRatings)))
+
+    m
 
 
 
