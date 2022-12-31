09203f19180054ee2571d23c69808eb86aa95053
Roman <zhijianliu@pm.me>
2023-01-01 00:01:27 +0800

    Pure Ambition
    Pure aggressive 002

--- Inner Peace/01GMF_test.py
+++ Inner Peace/01GMF_test.py
@@ -128,7 +128,7 @@
     
     # Init performance
     # t1 = time()
-    (hits, ndcgs) = evaluate_model(model, testRatings, testNegatives, topK, evaluation_threads)
+    # (hits, ndcgs) = evaluate_model(model, testRatings, testNegatives, topK, evaluation_threads)
     hr, ndcg = np.array(hits).mean(), np.array(ndcgs).mean()
     #mf_embedding_norm = np.linalg.norm(model.get_layer('user_embedding').get_weights())+np.linalg.norm(model.get_layer('item_embedding').get_weights())
     #p_norm = np.linalg.norm(model.get_layer('prediction').get_weights()[0])
