0d608dfe62958f3ed9ad5bed41bb033cb87e5397
Roman <zhijianliu@pm.me>
2023-01-03 07:18:24 +0800

    Pure Ambition
    Pure aggressive 017

--- Inner Peace/01GMF.py
+++ Inner Peace/01GMF.py
@@ -115,7 +115,7 @@ if __name__ == '__main__':
     (hits, ndcgs) = evaluate_model(model, testRatings, testNegatives, topK, evaluation_threads)
     hr, ndcg = np.array(hits).mean(), np.array(ndcgs).mean()
 
-    print('Init: HR')
+    print('Init: HR ')
 
 
 
