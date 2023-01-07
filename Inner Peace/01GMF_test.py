78452b165539e7286c4951777a6150fbf743ee60
Roman <zhijianliu@pm.me>
2023-01-07 12:03:26 +0800

    Pure Ambition
    Pure aggressive 005

--- Inner Peace/01GMF_test.py
+++ Inner Peace/01GMF_test.py
@@ -135,7 +135,7 @@
     # print('Init: HR = %.4f, NDCG = %.4f\t [%.1f s]' % (hr, ndcg, time()-t1))
     
     # Train model
-    best_hr, best_ndcg, best_iter = hr, ndcg, -1
+    # best_hr, best_ndcg, best_iter = hr, ndcg, -1
     for epoch in xrange(epochs):
         t1 = time()
         # Generate training instances
