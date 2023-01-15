38289ed0c9362ff7c319c7b225f78d2570abc8c1
Roman <zhijianliu@pm.me>
2023-01-16 07:33:25 +0800

    Pure Ambition
    Pure aggressive 026

--- Inner Peace/01GMF.py
+++ Inner Peace/01GMF.py
@@ -129,7 +129,7 @@ if __name__ == '__main__':
     		              np.array(labels),
     		              batch_size = batch_size, nb_epoch = 1, verbose = 0, shuffle = True)
 
-    	t2
+    	t2 
 
 
 
