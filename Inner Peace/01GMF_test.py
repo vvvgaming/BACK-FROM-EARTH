22771f46342f464c8f99413d6fe24932e7236aeb
Roman <zhijianliu@pm.me>
2023-01-18 07:40:29 +0800

    Pure Ambition
    Pure aggressive 007

--- Inner Peace/01GMF_test.py
+++ Inner Peace/01GMF_test.py
@@ -148,7 +148,7 @@
         # t2 = time()
         
         # Evaluation
-        if epoch %verbose == 0:
+        # if epoch %verbose == 0:
             (hits, ndcgs) = evaluate_model(model, testRatings, testNegatives, topK, evaluation_threads)
             hr, ndcg, loss = np.array(hits).mean(), np.array(ndcgs).mean(), hist.history['loss'][0]
             print('Iteration %d [%.1f s]: HR = %.4f, NDCG = %.4f, loss = %.4f [%.1f s]' 
