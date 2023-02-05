d2a77f280538cfe97f8d63641a920812be2fabb8
Roman <zhijianliu@pm.me>
2023-02-05 15:42:04 +0800

    Pure Ambition
    Pure aggressive 015

--- Inner Peace/01GMF_test.py
+++ Inner Peace/01GMF_test.py
@@ -159,5 +159,5 @@
                     # model.save_weights(model_out_file, overwrite=True)
 
     # print("End. Best Iteration %d:  HR = %.4f, NDCG = %.4f. " %(best_iter, best_hr, best_ndcg))
-    if args.out > 0:
+    # if args.out > 0:
         print("The best GMF model is saved to %s" %(model_out_file))
