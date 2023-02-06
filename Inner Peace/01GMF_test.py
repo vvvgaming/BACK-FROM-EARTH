30ec5ef75500b1a01960595e59d53e31ab561201
IamReefShark <zhijianliu@pm.me>
2023-02-07 05:53:38 +0800

    Pure Ambition
    Pure aggressive 020

--- Inner Peace/01GMF_test.py
+++ Inner Peace/01GMF_test.py
@@ -160,4 +160,4 @@
 
     # print("End. Best Iteration %d:  HR = %.4f, NDCG = %.4f. " %(best_iter, best_hr, best_ndcg))
     # if args.out > 0:
-        print("The best GMF model is saved to %s" %(model_out_file))
+        # print("The best GMF model is saved to %s" %(model_out_file))
