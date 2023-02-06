fda16e30d311ff699845d3cd2c9ca7b7deb01ee9
IamReefShark <zhijianliu@pm.me>
2023-02-07 05:54:57 +0800

    Pure Ambition
    Pure aggressive 026

--- Inner Peace/01GMF_test.py
+++ Inner Peace/01GMF_test.py
@@ -76,10 +76,5 @@
     # user_input, item_input, labels = [],[],[]
     # num_users = train.shape[0]
     # for (u, i) in train.keys():
-        # positive instance
-        # user_input.append(u)
-        # item_input.append(i)
-        # labels.append(1)
-        # negative instances
 
 
