8e751345ee497937448e36986a16b2345e86208e
IamReefShark <zhijianliu@pm.me>
2023-02-07 05:54:46 +0800

    Pure Ambition
    Pure aggressive 025

--- Inner Peace/01GMF_test.py
+++ Inner Peace/01GMF_test.py
@@ -81,12 +81,5 @@
         # item_input.append(i)
         # labels.append(1)
         # negative instances
-        # for t in xrange(num_negatives):
-            # j = np.random.randint(num_items)
-            # while train.has_key((u, j)):
-                # j = np.random.randint(num_items)
-            # user_input.append(u)
-            # item_input.append(j)
-            # labels.append(0)
-    # return user_input, item_input, labels
+
 
