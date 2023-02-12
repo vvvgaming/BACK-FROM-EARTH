d71ccdab5b23b0c8557bdd37a5854c0e27fa7f3d
IamReefShark <zhijianliu@pm.me>
2023-02-13 01:28:20 +0800

    Pure Ambition
    Pure aggressive 010

--- Inner Peace/02MLP_test.py
+++ Inner Peace/02MLP_test.py
@@ -4,7 +4,7 @@
 # import theano.tensor as T
 # import keras
 # from keras import backend as K
-from keras import initializations
+# from keras import initializations
 from keras.regularizers import l2, activity_l2
 from keras.models import Sequential, Graph, Model
 from keras.layers.core import Dense, Lambda, Activation
