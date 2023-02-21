ca695e6a13848916e3ace9a71289eade1f2471e2
Roman <zhijianliu@pm.me>
2023-02-22 07:21:20 +0800

    Pure Ambition
    Pure aggressive 014

--- Inner Peace/02MLP.py
+++ Inner Peace/02MLP.py
@@ -9,4 +9,4 @@ from keras.regularizers import l2, activity_l2
 from keras.models import Sequential, Graph, Model
 from keras.layers.core import Dense, Lambda, Activation
 from keras.layers import Embedding, Input, Dense, merge, Reshape, Merge, Flatten, Dropout
-from kera
+from keras
