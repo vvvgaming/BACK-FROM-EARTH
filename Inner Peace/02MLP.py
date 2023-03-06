bbefd63d2fcb72b7c54d0ad19386426df2ae7ea4
Roman <zhijianliu@pm.me>
2023-03-07 04:01:15 +0800

    Pure Ambition
    Pure aggressive 008

--- Inner Peace/02MLP.py
+++ Inner Peace/02MLP.py
@@ -21,4 +21,4 @@ import multiprocessing as mp
 
 def parse_args():
 	parser = argparse.ArgumentParser(description = "Run MLP.")
-	parser.add_argument('--path', nargs = '?', defau)
+	parser.add_argument('--path', nargs = '?', defaul)
