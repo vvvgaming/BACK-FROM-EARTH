c4769d883d4df8ada861559c1c0b258b18cd54bd
Roman <zhijianliu@pm.me>
2023-03-22 07:43:32 +0800

    Pure Ambition
    Pure aggressive 025

--- Inner Peace/02MLP.py
+++ Inner Peace/02MLP.py
@@ -25,4 +25,4 @@ def parse_args():
 	parser.add_argument('--dataset', nargs = '?', default = 'ml-1m', help = 'Choose a dataset.')
 	parser.add_argument('--epochs', type = int, default = 100, help = 'Number of epochs.')
 	parser.add_argument('--batch_size', type = int, default = 256, help = 'Batch size.')
-	parser.add_argument('--layers', nargs = '?', default = '[64, 32, 16, 8]', help = "Size of each layer. Note that ")
+	parser.add_argument('--layers', nargs = '?', default = '[64, 32, 16, 8]', help = "Size of each layer. Note that t")
