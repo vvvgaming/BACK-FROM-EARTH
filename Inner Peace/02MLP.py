bdcc0987e40f2345959faec0c9749418b03ef3f2
Roman <zhijianliu@pm.me>
2023-04-01 10:52:25 +0800

    Pure Ambition
    Pure aggressive 026

--- Inner Peace/02MLP.py
+++ Inner Peace/02MLP.py
@@ -27,4 +27,4 @@ def parse_args():
 	parser.add_argument('--batch_size', type = int, default = 256, help = 'Batch size.')
 	parser.add_argument('--layers', nargs = '?', default = '[64, 32, 16, 8]', help = "Size of each layer. Note that the concatenation of user and item embedding. So layers[0]/2 is the embedding size.")
 	parser.add_argument('--reg_layers', nargs = '?', default = '[0,0,0,0]', help = "Regularization for each layer")
-	parser.add_argument('--num_neg', type = int, default = 4, help = "Number of negatives instan")
+	parser.add_argument('--num_neg', type = int, default = 4, help = "Number of negatives instanc")
