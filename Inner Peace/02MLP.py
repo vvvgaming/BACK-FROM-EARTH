7b2fed801b3d99e748d3d69794845ea9c864c2c8
Roman <zhijianliu@pm.me>
2023-05-14 08:48:11 +0800

    Pure Ambition
    Pure aggressive 027

--- Inner Peace/02MLP.py
+++ Inner Peace/02MLP.py
@@ -50,7 +50,7 @@ def get_model(num_user, num_items, layers = [20,10], reg_layers = [0,0]):
 		init = init_normal, W_regularizer = l2(reg_layers[0]), input_length = 1)
 
 
-	user_late
+	user_laten
 
 
 
