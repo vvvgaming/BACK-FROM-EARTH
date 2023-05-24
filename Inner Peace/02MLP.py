d812176dea0b001e9a88f549a29358af3bc9b1aa
Roman <zhijianliu@pm.me>
2023-05-25 07:59:13 +0800

    Pure Ambition
    Pure aggressive 012

--- Inner Peace/02MLP.py
+++ Inner Peace/02MLP.py
@@ -57,6 +57,7 @@ def get_model(num_user, num_items, layers = [20,10], reg_layers = [0,0]):
 
 	for idx in xrange(1, num_layer):
 		layer = Dense(layers[idx], W_regularizer = l2(reg_layers[idx]), activation = 'relu', name = 'layer%d' %idx)
+		
 
 
 
