2b864dc389226690eb79e5d934692d3b59a4533d
IamReefShark <zhijianliu@pm.me>
2023-06-13 01:39:42 +0800

    Pure Ambition
    Pure aggressive 005

--- Inner Peace/02MLP_test.py
+++ Inner Peace/02MLP_test.py
@@ -92,7 +92,7 @@
 #         for t in xrange(num_negatives):
 #             j = np.random.randint(num_items)
             # while train.has_key((u, j)):
-                j = np.random.randint(num_items)
+                # j = np.random.randint(num_items)
             user_input.append(u)
             item_input.append(j)
             labels.append(0)
