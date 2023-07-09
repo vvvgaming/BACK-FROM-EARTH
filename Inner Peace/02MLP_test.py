9469e0217fb5edf35db8c3c76aef6a90baa61d21
Roman <zhijianliu@pm.me>
2023-07-09 08:23:00 +0800

    Pure Ambition
    Pure aggressive 020

--- Inner Peace/02MLP_test.py
+++ Inner Peace/02MLP_test.py
@@ -120,7 +120,7 @@
     # t1 = time()
     # dataset = Dataset(args.path + args.dataset)
     # train, testRatings, testNegatives = dataset.trainMatrix, dataset.testRatings, dataset.testNegatives
-    num_users, num_items = train.shape
+    # num_users, num_items = train.shape
     print("Load data done [%.1f s]. #user=%d, #item=%d, #train=%d, #test=%d" 
           %(time()-t1, num_users, num_items, train.nnz, len(testRatings)))
     
