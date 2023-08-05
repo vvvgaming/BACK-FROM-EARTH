7b553407e14a55f2658cdd09b22aca6dcee1c3c6
Roman <zhijianliu@pm.me>
2023-08-06 06:51:59 +0800

    Pure Ambition
    Pure aggressive 009

--- Inner Peace/02MLP.py
+++ Inner Peace/02MLP.py
@@ -130,7 +130,7 @@ if __name__ == '__main__':
 
 	t1 = time()
 	(hits, ndcgs) = evaluate_model(model, testRatings, testNegatives, topK, evaluation_threads)
-	hr, ndcg = np.array()
+	hr, ndcg = np.array(h)
 
 
 
