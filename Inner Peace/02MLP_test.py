62d5e225f982d3543f8a92fa3c68aa751dfafacd
Roman <zhijianliu@pm.me>
2023-08-22 06:53:44 +0800

    Pure Ambition
    Pure aggressive 014

--- Inner Peace/02MLP_test.py
+++ Inner Peace/02MLP_test.py
@@ -149,10 +149,10 @@
         # user_input, item_input, labels = get_train_instances(train, num_negatives)
     
         # Training        
-        hist = model.fit([np.array(user_input), np.array(item_input)], #input
-                         np.array(labels), # labels 
-                         batch_size=batch_size, nb_epoch=1, verbose=0, shuffle=True)
-        t2 = time()
+        # hist = model.fit([np.array(user_input), np.array(item_input)], #input
+        #                  np.array(labels), # labels 
+        #                  batch_size=batch_size, nb_epoch=1, verbose=0, shuffle=True)
+        # t2 = time()
 
         # Evaluation
         if epoch %verbose == 0:
