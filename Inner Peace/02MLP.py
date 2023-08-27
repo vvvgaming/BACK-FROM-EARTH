ae0ccb3d44a78f95f84ca8b3ffae49e1d1e275cc
Roman <zhijianliu@pm.me>
2023-08-28 05:25:37 +0800

    Pure Ambition
    Pure aggressive 007

--- Inner Peace/02MLP.py
+++ Inner Peace/02MLP.py
@@ -147,7 +147,7 @@ if __name__ == '__main__':
 
 
 		if epoch %verbose == 0:
-			(hits, ndcgs) = evaluate_model(model, testRatings, tesNe)
+			(hits, ndcgs) = evaluate_model(model, testRatings, tesNeg)
 
 
 
