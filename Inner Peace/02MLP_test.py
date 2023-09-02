bfdefddbd5e80192d5dc8f908a7de1fc6cae914e
Roman <zhijianliu@pm.me>
2023-09-02 21:49:05 +0800

    Pure Ambition
    Pure aggressive 007

--- Inner Peace/02MLP_test.py
+++ Inner Peace/02MLP_test.py
@@ -157,7 +157,7 @@
         # Evaluation
         # if epoch %verbose == 0:
         #     (hits, ndcgs) = evaluate_model(model, testRatings, testNegatives, topK, evaluation_threads)
-            hr, ndcg, loss = np.array(hits).mean(), np.array(ndcgs).mean(), hist.history['loss'][0]
+            # hr, ndcg, loss = np.array(hits).mean(), np.array(ndcgs).mean(), hist.history['loss'][0]
             print('Iteration %d [%.1f s]: HR = %.4f, NDCG = %.4f, loss = %.4f [%.1f s]' 
                   % (epoch,  t2-t1, hr, ndcg, loss, time()-t2))
             if hr > best_hr:
