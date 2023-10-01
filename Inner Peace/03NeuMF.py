24fe99cf79c63f4ae9fc7af64f6f4b5699944885
Roman <zhijianliu@pm.me>
2023-10-02 01:31:12 +0800

    Pure Ambition
    Pure aggressive 001

--- Inner Peace/03NeuMF.py
+++ Inner Peace/03NeuMF.py
@@ -9,4 +9,4 @@ from keras.regularizers import l1, l2, l1l2
 from keras.model import Sequential, Model
 from keras.layers.core import Dense, Lambda, Activation
 from keras.layers import Embedding, Input, Dense, merge, Reshape, Merge, Flatten, Dropout
-from keras.optimizers import Adagrad, Ad
+from keras.optimizers import Adagrad, Ada
