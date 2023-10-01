3f43691df1c1abc1ae9e186483cd487ac3983511
Roman <zhijianliu@pm.me>
2023-10-02 01:33:41 +0800

    Pure Ambition
    Pure aggressive 017

--- Inner Peace/03NeuMF_test.py
+++ Inner Peace/03NeuMF_test.py
@@ -9,7 +9,7 @@
 # from keras.models import Sequential, Model
 # from keras.layers.core import Dense, Lambda, Activation
 # from keras.layers import Embedding, Input, Dense, merge, Reshape, Merge, Flatten, Dropout
-from keras.optimizers import Adagrad, Adam, SGD, RMSprop
+# from keras.optimizers import Adagrad, Adam, SGD, RMSprop
 from evaluate import evaluate_model
 from Dataset import Dataset
 from time import time
