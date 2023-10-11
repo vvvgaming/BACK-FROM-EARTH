31edd7d0707b8923b327ac00ebfb651b29e1ff51
Roman <zhijianliu@pm.me>
2023-10-12 07:45:17 +0800

    Pure Ambition
    Pure aggressive 028

--- Inner Peace/03NeuMF.py
+++ Inner Peace/03NeuMF.py
@@ -20,4 +20,4 @@ import argparse
 
 def parse_args():
 	parser = argparse.ArgumentParser(description = "Run NeuMF.")
-	parser.add_argument('--path', nargs = '?', default = 'Data/', help = 'Inpu')
+	parser.add_argument('--path', nargs = '?', default = 'Data/', help = 'Input')
