2d15c4b00e5f66b1a9a3fa3be30930a8170f6e9f
Roman <zhijianliu@pm.me>
2023-10-21 08:54:37 +0800

    Pure Ambition
    Pure aggressive 012

--- Inner Peace/03NeuMF_test.py
+++ Inner Peace/03NeuMF_test.py
@@ -22,8 +22,8 @@
 #     parser = argparse.ArgumentParser(description="Run NeuMF.")
 #     parser.add_argument('--path', nargs='?', default='Data/',
 #                         help='Input data path.')
-    parser.add_argument('--dataset', nargs='?', default='ml-1m',
-                        help='Choose a dataset.')
+    # parser.add_argument('--dataset', nargs='?', default='ml-1m',
+    #                     help='Choose a dataset.')
     parser.add_argument('--epochs', type=int, default=100,
                         help='Number of epochs.')
     parser.add_argument('--batch_size', type=int, default=256,
