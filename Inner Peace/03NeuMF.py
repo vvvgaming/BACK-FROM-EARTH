827837517f821e5a4efe656a8390d6a286a8018d
Roman <zhijianliu@pm.me>
2023-11-16 07:35:32 +0800

    Pure Ambition
    s

--- Inner Peace/03NeuMF.py
+++ Inner Peace/03NeuMF.py
@@ -27,5 +27,5 @@ def parse_args():
 	parser.add_argument('--num_factors', type = int, default = 8, help = 'Embedding size of MF model.')
 	parser.add_argument("--layers', nargs = '?', default = '[64, 32, 16, 8]', help = 'MLP layers. Note that the first layer is the concatenation of user and item embeddings. So layers[0]/2 is the embedding size.")
 	parser.add_argument('--reg_mf', type = float, default = 0, help = 'Regularization for MF embeddings.')
-	parser.add_argument('--reg_layers', nargs = '?', default = '[0, 0, 0, 0]', help = "Regularization for")
+	parser.add_argument('--reg_layers', nargs = '?', default = '[0, 0, 0, 0]', help = "Regularization for ")
 
