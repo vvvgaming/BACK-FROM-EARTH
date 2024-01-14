40744126069622b5628fd90b0a3b4d0b9ffe11b3
vvvgaming <zhijianliu@pm.me>
2024-01-15 07:46:38 +0800

    Pure Ambition
    s

--- Inner Peace/03NeuMF.py
+++ Inner Peace/03NeuMF.py
@@ -52,7 +52,7 @@ def get_model(num_users, num_items, mf_dim = 10, layers = [10], reg_layers = [0]
 		init = init_normal, W_regularizer = l2(reg_mf), input_length = 1)
 	MF_Embedding_Item = Embedding(input_dim = num_items, output_dim = mf_dim, name = 'mf_embedding_item',
 		init = init_normal, W_regularizer = l2(reg_mf), input_length = 1)
-	MF_Embe
+	MF_Embed
 
 
 
