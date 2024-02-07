ca4fe8bf4d7e2553295cb3bd0379d789c4dcfc29
vvvgaming <zhijianliu@pm.me>
2024-02-08 07:21:18 +0800

    Pure Ambition
    s

--- Inner Peace/03NeuMF.py
+++ Inner Peace/03NeuMF.py
@@ -67,7 +67,7 @@ def get_model(num_users, num_items, mf_dim = 10, layers = [10], reg_layers = [0]
 
 	mlp_user_latent = Flatten()(MLF_Embedding_User(user_input))
 	mlp_item_latent = Flatten()(MLF_Embedding_Item(item_input))
-	mlp_vector = me
+	mlp_vector = mer
 
 
 
