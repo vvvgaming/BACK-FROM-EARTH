10f5831cb9c983313ab92453b80f26419054e353
vvvgaming <zhijianliu@pm.me>
2024-02-28 06:03:54 +0800

    Pure Ambition
    s

--- Inner Peace/03NeuMF.py
+++ Inner Peace/03NeuMF.py
@@ -81,7 +81,7 @@ def get_model(num_users, num_items, mf_dim = 10, layers = [10], reg_layers = [0]
 
 	model = Model(input = [user_input, item_input], output = prediction)
 
-	return mode
+	return model
 
 
 
