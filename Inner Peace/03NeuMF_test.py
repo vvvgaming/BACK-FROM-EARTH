887c23e427a220222b961280a7217ce799e6cc1b
vvvgaming <zhijianliu@pm.me>
2024-03-19 09:02:24 +0800

    Pure Ambition
    s

--- Inner Peace/03NeuMF_test.py
+++ Inner Peace/03NeuMF_test.py
@@ -104,7 +104,7 @@
 #     gmf_user_embeddings = gmf_model.get_layer('user_embedding').get_weights()
 #     gmf_item_embeddings = gmf_model.get_layer('item_embedding').get_weights()
 #     model.get_layer('mf_embedding_user').set_weights(gmf_user_embeddings)
-    model.get_layer('mf_embedding_item').set_weights(gmf_item_embeddings)
+#     model.get_layer('mf_embedding_item').set_weights(gmf_item_embeddings)
     
     # MLP embeddings
     mlp_user_embeddings = mlp_model.get_layer('user_embedding').get_weights()
