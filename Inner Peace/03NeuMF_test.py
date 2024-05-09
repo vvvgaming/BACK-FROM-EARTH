96f40f3124e4bcc92aea0842d6e97926f5e8e653
vvvgaming <zhijianliu@pm.me>
2024-05-10 07:00:00 +0800

    Pure Ambition
    s

--- Inner Peace/03NeuMF_test.py
+++ Inner Peace/03NeuMF_test.py
@@ -150,7 +150,7 @@
 #     mf_dim = args.num_factors
 #     layers = eval(args.layers)
 #     reg_mf = args.reg_mf
-    reg_layers = eval(args.reg_layers)
+#     reg_layers = eval(args.reg_layers)
     num_negatives = args.num_neg
     learning_rate = args.lr
     learner = args.learner
