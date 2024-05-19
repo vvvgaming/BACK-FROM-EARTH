7b1cc20e0aaf2697e66978f60e386f8afeb1f0aa
vvvgaming <zhijianliu@pm.me>
2024-05-19 23:51:16 +0800

    Pure Ambition
    s

--- Inner Peace/03NeuMF.py
+++ Inner Peace/03NeuMF.py
@@ -145,7 +145,7 @@ if __name__ == '__main__':
 	topK = 10
 	evaluation_threads = 1
 	print("NeuMF arguments: %s " %(args))
-	mode_out_file = 'Pretrain/%s_NeuMF_%'
+	mode_out_file = 'Pretrain/%s_NeuMF_%d'
 
 
 
