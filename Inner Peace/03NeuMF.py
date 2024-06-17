12d8d4cf0d790fefa8a81490174a702b172d79ce
vvvgaming <zhijianliu@pm.me>
2024-06-17 15:41:40 +0800

    Pure Ambition
    s

--- Inner Peace/03NeuMF.py
+++ Inner Peace/03NeuMF.py
@@ -162,7 +162,7 @@ if __name__ == '__main__':
 	elif learner.lower() == "rmsprop":
 		model.compile(optimizer = RMSprop(lr = learning_rate), loss = 'binary_crossentropy')
 	elif learner.lower() == 'adam':
-		model.compile(optimizer )
+		model.compile(optimizer =)
 
 
 
