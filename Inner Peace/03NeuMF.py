f5abc753d231f171395ec108f3bb47641f92a413
vvvgaming <zhijianliu@pm.me>
2024-06-29 09:23:47 +0800

    Pure Ambition
    s

--- Inner Peace/03NeuMF.py
+++ Inner Peace/03NeuMF.py
@@ -171,7 +171,7 @@ if __name__ == '__main__':
 		gmf_model = GMF.get_model(num_users, num_items, mf_dim)
 		gmf_model.load_weights(mf_pretrain)
 		mlp_model = MLP.get_model(num_users, num_items, layers, reg_layers)
-		mlp_mode
+		mlp_model
 
 
 
