7063e5057192e8c7398c565c5d293eb27a5b05b4
vvvgaming <zhijianliu@pm.me>
2024-07-08 06:02:44 +0800

    Pure Ambition
    s

--- Inner Peace/03NeuMF.py
+++ Inner Peace/03NeuMF.py
@@ -176,7 +176,7 @@ if __name__ == '__main__':
 		print("Load pretrained GMF (%s) and MLP (%s) models done. " %(mf_pretrain, mlp_pretrain))
 
 
-	(hits, ndcgs) = evaluate_model(model, testRatings, testNegatives, topK, evaluat)
+	(hits, ndcgs) = evaluate_model(model, testRatings, testNegatives, topK, evaluati)
 
 
 
