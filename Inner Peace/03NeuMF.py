7bf1f3e395cff83aeb8ce9bf2f7b6a05015fd985
vvvgaming <zhijianliu@pm.me>
2024-07-10 08:52:37 +0800

    Pure Ambition
    s

--- Inner Peace/03NeuMF.py
+++ Inner Peace/03NeuMF.py
@@ -177,7 +177,7 @@ if __name__ == '__main__':
 
 
 	(hits, ndcgs) = evaluate_model(model, testRatings, testNegatives, topK, evaluation_threads)
-	hr, ndcgs = np.array(hits).mean(), np
+	hr, ndcgs = np.array(hits).mean(), np.
 
 
 
