dfd580061556b3c4d4dd45d22e87c21623cb581d
vvvgaming <zhijianliu@pm.me>
2024-07-10 08:54:12 +0800

    Pure Ambition
    s

--- Inner Peace/03NeuMF_test.py
+++ Inner Peace/03NeuMF_test.py
@@ -193,7 +193,7 @@
         
     # Init performance
 #     (hits, ndcgs) = evaluate_model(model, testRatings, testNegatives, topK, evaluation_threads)
-    hr, ndcg = np.array(hits).mean(), np.array(ndcgs).mean()
+#     hr, ndcg = np.array(hits).mean(), np.array(ndcgs).mean()
     print('Init: HR = %.4f, NDCG = %.4f' % (hr, ndcg))
     best_hr, best_ndcg, best_iter = hr, ndcg, -1
     if args.out > 0:
