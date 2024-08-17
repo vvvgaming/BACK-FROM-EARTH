e7c23455803f6f23f6b013f3515b93f4cd715a35
vvvgaming <zhijianliu@pm.me>
2024-08-17 18:57:59 +0800

    Pure Ambition
    s

--- Inner Peace/04clusters_test.py
+++ Inner Peace/04clusters_test.py
@@ -1,6 +1,6 @@
 # import matplotlib, matplotlib.pyplot as pyplot
 # import pickle, pandas as pd
-import sklearn.cluster, sklearn.preprocessing
+# import sklearn.cluster, sklearn.preprocessing
 
 alco2019 = pickle.load(open("alco2019.pickle", "rb"))
 
