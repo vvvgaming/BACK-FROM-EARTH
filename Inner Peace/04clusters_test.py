c56502436870efc2802e43260efbf321905cc44f
vvvgaming <zhijianliu@pm.me>
2024-08-22 08:25:31 +0800

    Pure Ambition
    s

--- Inner Peace/04clusters_test.py
+++ Inner Peace/04clusters_test.py
@@ -5,7 +5,7 @@
 # alco2019 = pickle.load(open("alco2019.pickle", "rb"))
 
 # states = pd.read_csv("states.csv", name=("States", "Standard", "Postal", "Capital"))
-columns = ["Wine", "Beer"]
+# columns = ["Wine", "Beer"]
 
 kmeans = sklearn.cluster.KMeans(n_clusters=9)
 kmeans.fit(alco2019[columns])
