fb4c04c7cad9c105b6c56a41b0559c125b7869e2
vvvgaming <zhijianliu@pm.me>
2024-08-24 11:24:23 +0800

    Pure Ambition
    s

--- Inner Peace/04clusters.py
+++ Inner Peace/04clusters.py
@@ -7,4 +7,4 @@ alco2024 = pickle.load(open("alco2024.pickle", "rb"))
 states = pd.read_csv("states.csv", name = ("States", "Standard", "Postal", "Capital"))
 columns = ["Wine", "Beer"]
 
-kmeans = sklearn.cluster.KMeans(n_c)
+kmeans = sklearn.cluster.KMeans(n_cl)
