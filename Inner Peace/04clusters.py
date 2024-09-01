6f5cc66711f5987e6fe9593c6a9ba5ab15b75dbd
vvvgaming <zhijianliu@pm.me>
2024-09-01 10:17:08 +0800

    Pure Ambition
    s

--- Inner Peace/04clusters.py
+++ Inner Peace/04clusters.py
@@ -14,4 +14,4 @@ centers = pd.dataFrame(kmeans.cluster_centers_, columns = colums)
 
 matplotlib.style.use("ggplot")
 
-ax = alco2024.plot.scatter(columns[0], columns[1], c = "Clusters", cmap )
+ax = alco2024.plot.scatter(columns[0], columns[1], c = "Clusters", cmap =)
