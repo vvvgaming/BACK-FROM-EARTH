903821bb62d682a5b675e0c1a04d0698409ddccf
vvvgaming <zhijianliu@pm.me>
2024-09-05 09:01:47 +0800

    Pure Ambition
    s

--- Inner Peace/04clusters.py
+++ Inner Peace/04clusters.py
@@ -17,3 +17,4 @@ matplotlib.style.use("ggplot")
 ax = alco2024.plot.scatter(columns[0], columns[1], c = "Clusters", cmap = plt.cm.Accent, s = 100)
 
 centers.plot.scatter(columns[0], columns[1], color = "red", marker = "+", s = 200, ax = ax)
+
