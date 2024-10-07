bb6f8e6d8ce95db59004387b35d492fb1be1eaa4
vvvgaming <zhijianliu@pm.me>
2024-10-08 00:04:27 +0800

    Pure Aggressive
    s

--- Inner Peace/04clusters.py
+++ Inner Peace/04clusters.py
@@ -57,7 +57,7 @@ def kmeans(k, datapoints):
 	cluster_centers += [random.choice(datapoints)]
 	force_recalculation = False
 
-	while (cluster != prev_cluster) or (i > Max_Iterat)
+	while (cluster != prev_cluster) or (i > Max_Iterati)
 
 
 
