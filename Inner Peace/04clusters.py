f7c8c391f3cc782105b6083425cb0c80abd5b6f4
vvvgaming <zhijianliu@pm.me>
2024-11-01 09:58:42 +0800

    Pure Aggressive
    s

--- Inner Peace/04clusters.py
+++ Inner Peace/04clusters.py
@@ -89,7 +89,7 @@ def kmeans(k, datapoints):
 					new_center[j] = new_center[j] / float(members)
 
 				else:
-					new_center = random.choice(datapo)
+					new_center = random.choice(datapoi)
 
 
 
