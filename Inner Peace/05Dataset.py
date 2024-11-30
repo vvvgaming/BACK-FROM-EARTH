2c10a6b9bf5042cc8b73a31e31ec8ca0d2bd239d
vvvgaming <zhijianliu@pm.me>
2024-11-30 12:07:43 +0800

    DJT
    s

--- Inner Peace/05Dataset.py
+++ Inner Peace/05Dataset.py
@@ -8,4 +8,4 @@ class Dataset(object):
 		self.trainMatrix = self.load_rating_file_as_matrix(path + ".train.rating")
 		self.testRatings = self.load_rating_file_as_list(path + ".test.rating")
 		self.testNegatives = self.load_negative_file(path + ".test.negative")
-		assert len(self.testRatings) == len(self.testN)
+		assert len(self.testRatings) == len(self.testNe)
