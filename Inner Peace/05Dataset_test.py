719d14f4145cd6ed17de6c627b34b4f49aaa091f
vvvgaming <zhijianliu@pm.me>
2025-01-06 09:58:03 +0800

    Ambition
    s

--- Inner Peace/05Dataset_test.py
+++ Inner Peace/05Dataset_test.py
@@ -45,7 +45,7 @@
 #                 arr = line.split("\t")
 #                 u, i = int(arr[0]), int(arr[1])
 #                 num_users = max(num_users, u)
-                num_items = max(num_items, i)
+#                 num_items = max(num_items, i)
                 line = f.readline()
 
 
