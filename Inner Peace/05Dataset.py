01cc28c3b0ef47e99482988024af8163c6672db6
vvvgaming <zhijianliu@pm.me>
2025-01-10 09:02:45 +0800

    Ambition
    s

--- Inner Peace/05Dataset.py
+++ Inner Peace/05Dataset.py
@@ -49,7 +49,7 @@ class Dataset(object):
 				line = f.readline()
 
 
-        mat = sp.dok_matrix((num_users + 1, num_items + 1), dt)
+        mat = sp.dok_matrix((num_users + 1, num_items + 1), dty)
 
 
 
