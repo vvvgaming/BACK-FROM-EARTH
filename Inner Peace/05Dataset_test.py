ce07503494bca9aed30e298306ba8a079abe3b9d
vvvgaming <zhijianliu@pm.me>
2025-01-11 08:53:02 +0800

    Ambition
    s

--- Inner Peace/05Dataset_test.py
+++ Inner Peace/05Dataset_test.py
@@ -49,7 +49,7 @@
 #                 line = f.readline()
 
 
-        mat = sp.dok_matrix((num_users+1, num_items+1), dtype = np.float32)
+#         mat = sp.dok_matrix((num_users+1, num_items+1), dtype = np.float32)
         with open(filename, "r") as f:
             line = f.readline()
             while line != None and line != "":
