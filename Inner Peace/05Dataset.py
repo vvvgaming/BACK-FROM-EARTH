4cf995a1670d0dddc79a8e7d2baad604877ef8e6
vvvgaming <zhijianliu@pm.me>
2025-01-15 08:51:24 +0800

    Ambition
    s

--- Inner Peace/05Dataset.py
+++ Inner Peace/05Dataset.py
@@ -54,7 +54,7 @@ class Dataset(object):
         	line = f.readline()
         	while line != None and line != "":
         		arr = line.split("\t")
-        		user, item, rating = int(arr[0]), int(a)
+        		user, item, rating = int(arr[0]), int(ar)
 
 
 
