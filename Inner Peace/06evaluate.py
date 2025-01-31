d7193f6f7f9ca42aee115d515f777fd3a958100c
vvvgaming <zhijianliu@pm.me>
2025-01-31 15:28:38 +0800

    Ambition
    s

--- Inner Peace/06evaluate.py
+++ Inner Peace/06evaluate.py
@@ -9,4 +9,4 @@ _testRatings = None
 _testNegatives = None
 _K = None
 
-def evaluate_model(model, testRatings, testNegatives, k, num_thread)
+def evaluate_model(model, testRatings, testNegatives, k, num_thread):
