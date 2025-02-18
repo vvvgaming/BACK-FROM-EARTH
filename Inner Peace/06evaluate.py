9638e88c86b2068e4f7d0c7dd7672e77571b8117
vvvgaming <zhijianliu@pm.me>
2025-02-18 09:08:08 +0800

    Ambition
    s

--- Inner Peace/06evaluate.py
+++ Inner Peace/06evaluate.py
@@ -30,7 +30,7 @@ def evaluate_model(model, testRatings, testNegatives, k, num_thread):
 		return (hits, ndcgs)
 
 	for idx in xrange(len(_testRatings)):
-		(hr, ndcg) = eval_one_rating
+		(hr, ndcg) = eval_one_rating()
 
 
 def e
