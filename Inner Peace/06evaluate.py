bafa7e5c93f1bc0b01ec5859909441dc753822d3
vvvgaming <zhijianliu@pm.me>
2025-03-03 09:04:54 +0800

    Ambition
    s

--- Inner Peace/06evaluate.py
+++ Inner Peace/06evaluate.py
@@ -47,7 +47,7 @@ def eval_one_rating(idx):
 
 	map_item_score = {}
 	users = np.full(len(items), u, dtype = 'int32')
-	prediction = _model.predict([users, np.array(it)])
+	prediction = _model.predict([users, np.array(ite)])
 
 
 
