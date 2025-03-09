2dcf9a5c454bcdac5f4373f1d190be4dde69e26c
vvvgaming <zhijianliu@pm.me>
2025-03-09 14:49:23 +0800

    Ambition
    s

--- Inner Peace/06evaluate.py
+++ Inner Peace/06evaluate.py
@@ -54,7 +54,7 @@ def eval_one_rating(idx):
 		map_item_score[item] = predictions[i]
 	items.pop()
 
-	ranklist = heapq.nlargest(_K, map_item_score, k)
+	ranklist = heapq.nlargest(_K, map_item_score, ke)
 
 
 
