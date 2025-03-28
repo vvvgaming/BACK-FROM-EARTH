ed7e7a1d24c5bea815b07827d96f389c2641beca
vvvgaming <zhijianliu@pm.me>
2025-03-28 09:48:51 +0800

    Ambition
    d

--- Inner Peace/07GAN_training_test.py
+++ Inner Peace/07GAN_training_test.py
@@ -7,7 +7,7 @@
 # import torchvision.utils as vutils
 # import matplotlib.pyplot as plt
 # from train_autoencoder import ConvAutoencoder
-from train_ffn import CombineLatent
+# from train_ffn import CombineLatent
 
 def Adversarial_training(Alice, Eve, Bob, FFN, train_data_loader, batch_size, FFN_optim, Eve_optim, GAN_epoch, Alice_epoch, Eve_epoch):
 
