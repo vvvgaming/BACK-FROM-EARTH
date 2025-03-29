7cd39dd6d185a03d6758555c7285a2b4e594d7c2
vvvgaming <zhijianliu@pm.me>
2025-03-30 00:27:22 +0800

    Blaze
    Terra

--- Inner Peace/07GAN_training.py
+++ Inner Peace/07GAN_training.py
@@ -9,4 +9,4 @@ import matplotlib.pyplot as plt
 from train_autoencoder import ConvAutoencoder
 from train_ffn import CombineLatent
 
-def Adversarail_training(Alice, Roman, Eve, Bob, FFN,)
+def Adversarail_training(Alice, Roman, Eve, Bob, FFN, )
