9733655cff9e24e3bba3f2d79daae969cab51fe0
vvvgaming <zhijianliu@pm.me>
2025-04-14 09:26:40 +0800

    Blaze
    Terra

--- Inner Peace/07GAN_training.py
+++ Inner Peace/07GAN_training.py
@@ -24,3 +24,4 @@ def Adversarail_training(Alice, Roman, Eve, Bob, FFN, train_data_loader, batch_s
         if len(train_data[0]) != batch_size :
           continue
         arr = np.arange(batch_size)
+        
