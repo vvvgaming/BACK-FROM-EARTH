2c89c46786e03b332bb5a994c6a9ee1bbddf7f24
vvvgaming <zhijianliu@pm.me>
2025-04-28 08:16:36 +0800

    Blaze
    Terra

--- Inner Peace/07GAN_training.py
+++ Inner Peace/07GAN_training.py
@@ -36,7 +36,7 @@ def Adversarail_training(Alice, Roman, Eve, Bob, FFN, train_data_loader, batch_s
         oup_vec = oup_vec.reshape(-1, x, y, z)
         B_image = Bob.distract_latent(oup_vec)
         E_image = Eve.distract_latent(oup_vec)
-        criterion = 
+        criterion = n
 
 
 
