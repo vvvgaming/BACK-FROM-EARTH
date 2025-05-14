4bf5493bd79bee3bfb96ac4f9598608fe40261d7
vvvgaming <zhijianliu@pm.me>
2025-05-14 08:57:34 +0800

    Blaze
    Terra

--- Inner Peace/07GAN_training.py
+++ Inner Peace/07GAN_training.py
@@ -49,6 +49,7 @@ def Adversarail_training(Alice, Roman, Eve, Bob, FFN, train_data_loader, batch_s
       print("The Adversarial Loss for first phase is :", train_loss_ba / len(train_data_loader))
       if (train_loss_ba / len(train_data_loader)) < min_train_loss_ba :
         min_train_loss_ba = train_loss_ba / len(train_data_loader)
+        
 
 
 
