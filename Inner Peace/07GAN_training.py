abddd000c36ecd31314a702aaa691e36cf6d8353
vvvgaming <zhijianliu@pm.me>
2025-05-17 18:54:32 +0800

    Blaze
    Terra

--- Inner Peace/07GAN_training.py
+++ Inner Peace/07GAN_training.py
@@ -53,7 +53,7 @@ def Adversarail_training(Alice, Roman, Eve, Bob, FFN, train_data_loader, batch_s
 
 
     for z in range(Eve_epoch):
-      print("The Ep")
+      print("The Epo")
       train_loss_e
 
 
