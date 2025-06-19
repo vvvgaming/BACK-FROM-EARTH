344647c04f375bd4669888b37acc42e8f915b32a
vvvgaming <zhijianliu@pm.me>
2025-06-20 01:25:43 +0800

    Blaze
    Terra

--- Inner Peace/07GAN_training.py
+++ Inner Peace/07GAN_training.py
@@ -84,7 +84,7 @@ def Adversarail_training(Alice, Roman, Eve, Bob, FFN, train_data_loader, batch_s
         train_loss_e += loss_item()
 
 
-      print("The Adversarial loss for second phase is :" , train_loss_ba / len())
+      print("The Adversarial loss for second phase is :" , train_loss_ba / len(t))
 
 
 
