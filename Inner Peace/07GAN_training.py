8704648d004fc6984ece71797b7cd58929d96ffb
vvvgaming <zhijianliu@pm.me>
2025-06-29 18:57:25 +0800

    Blaze
    Terra

--- Inner Peace/07GAN_training.py
+++ Inner Peace/07GAN_training.py
@@ -91,7 +91,7 @@ def Adversarail_training(Alice, Roman, Eve, Bob, FFN, train_data_loader, batch_s
 
 
 if __name__ == "__main__"
-    parser = argparse.ArgumentParser(description = 'Process some iteg')
+    parser = argparse.ArgumentParser(description = 'Process some itege')
 
 
 
