2102abfb8e4787b82da0e18070c291d3db2f248f
vvvgaming <zhijianliu@pm.me>
2025-11-18 00:07:32 +0800

    Windfall

--- Inner Peace/08GMF_test.py
+++ Inner Peace/08GMF_test.py
@@ -199,4 +199,4 @@ c3_observed_grid = [a_c3_list, b_c3_list]
 # chi2_test 3:
 a_c3_list = data_to_num_list(a_count_3) 
 b_c3_list = data_to_num_list(b_count_3)
-c3_observed_grid = [a_c3_list, b_c3_list
+c3_observed_grid = [a_c3_list, b_c3_lis
