c2e4f490e1e19196b440051c42ae32757d00f392
vvvgaming <zhijianliu@pm.me>
2025-12-02 19:24:08 +0800

    Windfall

--- Inner Peace/08GMF_test.py
+++ Inner Peace/08GMF_test.py
@@ -187,4 +187,4 @@ print(perform_chi2_homogeneity_test(c2_observed_grid)) # this should be 0.0000
 # chi2_test 3:
 a_c3_list = data_to_num_list(a_count_3) 
 b_c3_list = data_to_num_list(b_count_3)
-c3_observed_grid = [a_c3_list, b_c3_li
+c3_observed_grid = [a_c3_list, b_c3_l
