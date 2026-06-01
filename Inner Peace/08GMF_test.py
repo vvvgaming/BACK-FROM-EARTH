ab4ff0ff863dc77a34589f20d5c524dc542f6597
vvvgaming <zhijianliu@pm.me>
2026-06-02 00:04:25 +0800

    Scaling law

--- Inner Peace/08GMF_test.py
+++ Inner Peace/08GMF_test.py
@@ -171,4 +171,4 @@ print(perform_2_sample_t_test(a_t3_list, b_t3_list)) # this should be .005091
 """
 # chi2_test 1:
 a_c1_list = data_to_num_list(a_count_1) 
-b_c1_list = data_to_num_list(b_co
+b_c1_list = data_to_num_list(b_c
