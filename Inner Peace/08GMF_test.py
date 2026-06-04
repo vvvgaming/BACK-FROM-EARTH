61d3220412be6bbdf636766bdb672fd093dd11c6
vvvgaming <zhijianliu@pm.me>
2026-06-05 00:54:12 +0800

    Scaling law

--- Inner Peace/08GMF_test.py
+++ Inner Peace/08GMF_test.py
@@ -166,4 +166,3 @@ a_t3_list = data_to_num_list(a3)
 b_t3_list = data_to_num_list(b3)
 print(get_t_score(a_t3_list, b_t3_list)) # this should be -2.88969
 print(perform_2_sample_t_test(a_t3_list, b_t3_list)) # this should be .005091
-"
