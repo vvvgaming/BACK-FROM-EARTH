e83b40011d4982e6d42cc09041087816912c8036
vvvgaming <zhijianliu@pm.me>
2026-06-14 23:26:23 +0800

    Scaling law

--- Inner Peace/08GMF_test.py
+++ Inner Peace/08GMF_test.py
@@ -159,4 +159,4 @@ print(perform_2_sample_t_test(a_t1_list, b_t1_list)) # this should be 0.0000
 a_t2_list = data_to_num_list(a2) 
 b_t2_list = data_to_num_list(b2)
 print(get_t_score(a_t2_list, b_t2_list)) # this should be -1.48834
-print(perform_2_sample_t_test(a_t2_list, b_t2_list)) # 
+print(perform_2_sample_t_test(a_t2_list, b_t2_list)) #
