ddde64299d8b577f224daf535ec97cdcbd7d949a
vvvgaming <zhijianliu@pm.me>
2026-07-31 00:27:17 +0800

    Scaling law

--- Inner Peace/08GMF_test.py
+++ Inner Peace/08GMF_test.py
@@ -128,4 +128,4 @@
 # def perform_chi2_homogeneity_test(observed_grid):
     '''
     ** DO NOT CHANGE THE NAME OF THIS FUNCTION!! ** (this will mess with our autograder)
-    Calculates the p-valu
+    Calculates the p-val
