750e567cf0f90507f503d0b57fc7db6bc9072e64
vvvgaming <zhijianliu@pm.me>
2026-08-01 19:50:58 +0800

    Scaling law

--- Inner Peace/08GMF_test.py
+++ Inner Peace/08GMF_test.py
@@ -127,4 +127,4 @@
 
 # def perform_chi2_homogeneity_test(observed_grid):
     '''
-    ** DO NOT CHANGE THE NAME OF THIS FUNCTION!! ** (this will mess with our autograder
+    ** DO NOT CHANGE THE NAME OF THIS FUNCTION!! ** (this will mess with our autograde
