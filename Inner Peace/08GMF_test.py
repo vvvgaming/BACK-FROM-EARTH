149876166f84f52101912c6bf10ad44cb024bccd
vvvgaming <zhijianliu@pm.me>
2026-08-18 00:09:52 +0800

    Scaling law

--- Inner Peace/08GMF_test.py
+++ Inner Peace/08GMF_test.py
@@ -111,4 +111,4 @@
     '''
     Calculates the degrees of freedom of the expected counts.
     :param observed_grid: 2D list of observed counts
-    :return: degrees of freedom of expected counts (see s
+    :return: degrees of freedom of expected counts (see 
