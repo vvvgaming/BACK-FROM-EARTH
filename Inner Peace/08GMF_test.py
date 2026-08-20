40bbd41df0ecff2ad1dd220d66ed06eb92d04d05
vvvgaming <zhijianliu@pm.me>
2026-08-20 13:24:15 +0800

    Scaling law

--- Inner Peace/08GMF_test.py
+++ Inner Peace/08GMF_test.py
@@ -110,4 +110,4 @@
 # def df_chi2(observed_grid):
     '''
     Calculates the degrees of freedom of the expected counts.
-    :param observed_grid: 2D list of observed co
+    :param observed_grid: 2D list of observed c
