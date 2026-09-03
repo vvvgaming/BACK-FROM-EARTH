a07acb313ec93d2cf01eadaf678c972f53305dd2
vvvgaming <zhijianliu@pm.me>
2026-09-04 01:33:50 +0800

    Scaling law

--- Inner Peace/08GMF_test.py
+++ Inner Peace/08GMF_test.py
@@ -99,4 +99,4 @@
 # def get_expected_grid(observed_grid):
     '''
     Calculates the expected counts, given the observed counts.
-    ** DO NOT modify the parameter, observed_g
+    ** DO NOT modify the parameter, observed_
