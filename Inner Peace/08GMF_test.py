faa07e422ef1316c96bb0002f5dcad403b6b6756
vvvgaming <zhijianliu@pm.me>
2026-09-11 00:51:40 +0800

    Scaling law

--- Inner Peace/08GMF_test.py
+++ Inner Peace/08GMF_test.py
@@ -93,4 +93,4 @@
 # [OPTIONAL] Some helper functions that might be helpful in get_expected_grid().
 # def row_sum(observed_grid, ele_row):
 # def col_sum(observed_grid, ele_col):
-# def total_sum(observed_g
+# def total_sum(observed_
