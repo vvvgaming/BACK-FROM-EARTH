04c93dae328ab44519a73c25378d9e448da89f37
vvvgaming <zhijianliu@pm.me>
2026-09-19 19:45:44 +0800

    Scaling laws

--- Inner Peace/08GMF_test.py
+++ Inner Peace/08GMF_test.py
@@ -84,4 +84,4 @@
     :param a: list of numbers
     :param b: list of numbers
     :return: calculated p-value
-    HINT: the t_dist.cdf() function might com
+    HINT: the t_dist.cdf() function might co
