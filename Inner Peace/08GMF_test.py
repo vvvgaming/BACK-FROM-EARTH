81c5bcda0ee1c88574cdb3d2756605cd510fc47a
vvvgaming <zhijianliu@pm.me>
2026-09-25 00:18:12 +0800

    Scaling laws

--- Inner Peace/08GMF_test.py
+++ Inner Peace/08GMF_test.py
@@ -80,4 +80,4 @@
 # def perform_2_sample_t_test(a, b):
     '''
     ** DO NOT CHANGE THE NAME OF THIS FUNCTION!! ** (this will mess with our autograder)
-    Calculates a p-value by performing a 2-sample t-test, gi
+    Calculates a p-value by performing a 2-sample t-test, g
